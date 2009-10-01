6aba1ae5d416d8008bac37fb3aa06614bfe1e8f3
ThyMythos <ThyMythos@349629ee-b03d-11dd-8830-69e4bb380a4a>
2009-10-01 19:46:58 +0000

    New default map view that is suited for the end user. Has "My Location" overlay and standard zoom buttons. Uses SDK 1.6

---
+++ src/org/andnav/osm/OpenStreetMap.java
@@ -0,0 +1,223 @@
+// Created by plusminus on 00:23:14 - 03.10.2008
+package org.andnav.osm;
+
+import org.andnav.osm.util.GeoPoint;
+import org.andnav.osm.views.OpenStreetMapView;
+import org.andnav.osm.views.overlay.MyLocationOverlay;
+import org.andnav.osm.views.util.OpenStreetMapRendererInfo;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.SubMenu;
+import android.widget.RelativeLayout;
+import android.widget.ZoomButtonsController;
+import android.widget.RelativeLayout.LayoutParams;
+
+
+/**
+ * Default map view activity.
+ * @author Manuel Stahl
+ *
+ */
+public class OpenStreetMap extends Activity {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	private static final int MENU_MY_LOCATION = Menu.FIRST;
+	private static final int MENU_MAP_MODE = MENU_MY_LOCATION + 1;
+	private static final int MENU_ABOUT = MENU_MAP_MODE + 1;
+	
+	private static final int DIALOG_ABOUT_ID = 1;
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+	
+	private SharedPreferences mPrefs;
+	private OpenStreetMapView mOsmv;
+	private ZoomButtonsController zoomController;
+	private MyLocationOverlay mLocationOverlay;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    	mPrefs = getPreferences(MODE_PRIVATE);
+        
+        final RelativeLayout rl = new RelativeLayout(this);
+        
+        this.mOsmv = new OpenStreetMapView(this, OpenStreetMapRendererInfo.values()[mPrefs.getInt("renderer", OpenStreetMapRendererInfo.MAPNIK.ordinal())]);
+        this.mLocationOverlay = new MyLocationOverlay(this.getBaseContext(), this.mOsmv);
+        this.mOsmv.getOverlays().add(this.mLocationOverlay);
+        rl.addView(this.mOsmv, new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+        
+        
+        /* ZoomControls */
+        {
+	        zoomController = new ZoomButtonsController(mOsmv);	        
+	        zoomController.setOnZoomListener(new ZoomButtonsController.OnZoomListener() {
+	        	@Override
+	        	public void onZoom(boolean zoomIn) {
+	        		if(zoomIn)
+						OpenStreetMap.this.mOsmv.zoomIn();
+	        		else
+						OpenStreetMap.this.mOsmv.zoomOut();	        			
+	        	}
+	        	@Override
+	        	public void onVisibilityChanged(boolean visible) {}
+	        });
+	        
+	        zoomController.setZoomInEnabled(true);
+	        zoomController.setZoomOutEnabled(true);
+        }
+        
+        this.setContentView(rl);
+    
+    	mOsmv.setMapCenter(mPrefs.getInt("centerLat", 0), mPrefs.getInt("centerLon", 0));
+    	mOsmv.setZoomLevel(mPrefs.getInt("zoomLevel", 2));
+    	if(mPrefs.getBoolean("show_location", false))
+    		this.mLocationOverlay.enableMyLocation();
+    }
+    
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+    	super.onSaveInstanceState(outState);
+    	SharedPreferences.Editor edit = mPrefs.edit();
+    	GeoPoint mapCenter = mOsmv.getMapCenter();
+    	edit.putInt("renderer", mOsmv.getRenderer().ordinal());
+    	edit.putInt("centerLat", mapCenter.getLatitudeE6());
+    	edit.putInt("centerLon", mapCenter.getLongitudeE6());
+    	edit.putInt("zoomLevel", mOsmv.getZoomLevel());
+    	edit.putBoolean("show_location", mLocationOverlay.isMyLocationEnabled());
+    	edit.commit();
+    }
+        
+    @Override
+    protected void onPause() {
+    	this.mLocationOverlay.disableMyLocation();
+    	zoomController.setVisible(false);
+    	
+    	SharedPreferences.Editor edit = mPrefs.edit();
+    	GeoPoint mapCenter = mOsmv.getMapCenter();
+    	edit.putInt("renderer", mOsmv.getRenderer().ordinal());
+    	edit.putInt("centerLat", mapCenter.getLatitudeE6());
+    	edit.putInt("centerLon", mapCenter.getLongitudeE6());
+    	edit.putInt("zoomLevel", mOsmv.getZoomLevel());
+    	edit.putBoolean("show_location", mLocationOverlay.isMyLocationEnabled());
+    	edit.commit();
+    	super.onPause();
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	mOsmv.setRenderer(OpenStreetMapRendererInfo.values()[mPrefs.getInt("renderer", OpenStreetMapRendererInfo.MAPNIK.ordinal())]);
+    	if(mPrefs.getBoolean("show_location", false))
+    		this.mLocationOverlay.enableMyLocation();
+    }
+ 
+    @Override
+	public boolean onCreateOptionsMenu(final Menu pMenu) {
+    	pMenu.add(0, MENU_MY_LOCATION, Menu.NONE, R.string.my_location).setIcon(android.R.drawable.ic_menu_mylocation);
+
+    	{
+    	int id = 1000;
+			final SubMenu mapMenu = pMenu.addSubMenu(0, MENU_MAP_MODE,
+					Menu.NONE, R.string.map_mode).setIcon(
+					android.R.drawable.ic_menu_mapmode);
+
+			for (OpenStreetMapRendererInfo renderer : OpenStreetMapRendererInfo
+					.values()) {
+				mapMenu.add(MENU_MAP_MODE, id++, Menu.NONE, renderer.NAME);
+			}
+			mapMenu.setGroupCheckable(MENU_MAP_MODE, true, true);
+    	}
+    	
+    	pMenu.add(0, MENU_ABOUT, Menu.NONE, R.string.about).setIcon(android.R.drawable.ic_menu_info_details);
+    	
+    	return true;
+	}
+    
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+    	int id = mOsmv.getRenderer().ordinal();
+    	menu.findItem(1000 + id).setChecked(true);
+    	return true;
+    }
+	
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch(item.getItemId()) {
+			case MENU_MY_LOCATION:
+	    		this.mLocationOverlay.enableMyLocation();
+				return true;
+			
+			case MENU_MAP_MODE:
+				this.mOsmv.invalidate();
+				return true;
+				
+			case MENU_ABOUT:
+				showDialog(DIALOG_ABOUT_ID);
+				return true;
+								
+			default:	// Map mode submenu items 
+				this.mOsmv.setRenderer(OpenStreetMapRendererInfo.values()[item.getItemId() - 1000]);
+		}
+		return false;
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		Dialog dialog;
+
+		switch (id) {
+		case DIALOG_ABOUT_ID:
+			return new AlertDialog.Builder(OpenStreetMap.this)
+            .setIcon(R.drawable.icon)
+            .setTitle(R.string.about)
+            .setMessage(R.string.about_message)
+            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {}
+            }).create();
+
+		default:
+			dialog = null;
+			break;
+		}
+		return dialog;
+	}
+	
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods from SuperClass/Interfaces
+	// ===========================================================
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+    	if(event.getAction() == MotionEvent.ACTION_UP)
+    		zoomController.setVisible(true);
+    	return super.onTouchEvent(event);
+    }
+    
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
---
+++ src/org/andnav/osm/views/overlay/MyLocationOverlay.java
@@ -0,0 +1,190 @@
+// Created by plusminus on 22:01:11 - 29.09.2008
+package org.andnav.osm.views.overlay;
+
+import java.util.LinkedList;
+
+import org.andnav.osm.R;
+import org.andnav.osm.util.GeoPoint;
+import org.andnav.osm.util.TypeConverter;
+import org.andnav.osm.views.OpenStreetMapView;
+import org.andnav.osm.views.OpenStreetMapView.OpenStreetMapViewProjection;
+import org.andnav.osm.views.controller.OpenStreetMapViewController;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+
+/**
+ * 
+ * @author Manuel Stahl
+ *
+ */
+public class MyLocationOverlay extends OpenStreetMapViewOverlay implements LocationListener {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+	
+	protected final Paint mPaint = new Paint();
+	
+	protected final Bitmap DIRECTION_ARROW;
+	
+	protected OpenStreetMapView mMapView;
+	private Context mCtx;
+	private LocationManager mLocationManager;
+	private boolean mMyLocationEnabled = false;
+	private LinkedList<Runnable> mRunOnFirstFix = new LinkedList<Runnable>();
+	
+	protected Location mLocation;
+	
+	private final Matrix directionRotater = new Matrix();
+	
+	private final float DIRECTION_ARROW_CENTER_X;
+	private final float DIRECTION_ARROW_CENTER_Y;
+	private final int DIRECTION_ARROW_WIDTH;
+	private final int DIRECTION_ARROW_HEIGHT;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+	
+	public MyLocationOverlay(final Context ctx, final OpenStreetMapView mapView) {
+		this.mCtx = ctx;
+		this.mMapView = mapView;
+		
+		this.DIRECTION_ARROW = BitmapFactory.decodeResource(ctx.getResources(), R.drawable.direction_arrow);
+		
+		this.DIRECTION_ARROW_CENTER_X = this.DIRECTION_ARROW.getWidth() / 2 - 0.5f;
+		this.DIRECTION_ARROW_CENTER_Y = this.DIRECTION_ARROW.getHeight() / 2 - 0.5f;
+		this.DIRECTION_ARROW_HEIGHT = this.DIRECTION_ARROW.getHeight();
+		this.DIRECTION_ARROW_WIDTH = this.DIRECTION_ARROW.getWidth();
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+	
+	public Location getLastFix() {
+		return mLocation;
+	}
+	
+	public GeoPoint getMyLocation() {
+		return TypeConverter.locationToGeoPoint(mLocation);
+	}
+
+	public boolean isMyLocationEnabled() {
+		return mMyLocationEnabled;
+	}
+	
+	// ===========================================================
+	// Methods from SuperClass/Interfaces
+	// ===========================================================
+	
+	@Override
+	protected void onDrawFinished(Canvas c, OpenStreetMapView osmv) {
+		return;
+	}
+	
+	@Override
+	public void onDraw(final Canvas c, final OpenStreetMapView osmv) {
+		if(this.mLocation != null){
+			final OpenStreetMapViewProjection pj = osmv.getProjection();
+			final Point screenCoords = new Point();
+			pj.toPixels(TypeConverter.locationToGeoPoint(mLocation), screenCoords);
+			
+			
+			/* Rotate the direction-Arrow according to the bearing we are driving. And draw it to the canvas. */
+			this.directionRotater.setRotate(this.mLocation.getBearing(), DIRECTION_ARROW_CENTER_X , DIRECTION_ARROW_CENTER_Y);
+			Bitmap rotatedDirection = Bitmap.createBitmap(DIRECTION_ARROW, 0, 0, DIRECTION_ARROW_WIDTH, DIRECTION_ARROW_HEIGHT, this.directionRotater, false); 
+			c.drawBitmap(rotatedDirection, screenCoords.x - rotatedDirection.getWidth() / 2, screenCoords.y - rotatedDirection.getHeight() / 2, this.mPaint);
+		}
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		mLocation = location;
+		mMapView.getController().animateTo(TypeConverter.locationToGeoPoint(location),
+				OpenStreetMapViewController.AnimationType.HALFCOSINUSALDECELERATING,
+				OpenStreetMapViewController.ANIMATION_SMOOTHNESS_DEFAULT,
+				OpenStreetMapViewController.ANIMATION_DURATION_LONG);
+	}
+	
+	@Override
+	public void onProviderDisabled(String provider) {
+	}
+	
+	@Override
+	public void onProviderEnabled(String provider) {
+	}
+	
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		if(status == LocationProvider.AVAILABLE) {
+			final Thread t = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					for(Runnable runnable: mRunOnFirstFix) {
+						runnable.run();
+					}
+					mRunOnFirstFix.clear();
+				}
+			});
+			t.run();
+		}
+	}
+	
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	public void disableMyLocation() {
+		getLocationManager().removeUpdates(this);
+	}
+	
+	public boolean enableMyLocation() {
+		Criteria crit = new Criteria();
+		crit.setAccuracy(Criteria.ACCURACY_FINE);
+		
+		String provider = getLocationManager().getBestProvider(crit, true);
+		try {
+			getLocationManager().requestLocationUpdates(provider, 0, 0, this);
+		} catch(Exception e) {
+			disableMyLocation();
+			return mMyLocationEnabled = false;
+		}
+		return mMyLocationEnabled = true;
+	}
+	
+	public boolean runOnFirstFix(Runnable runnable) {
+		if(mMyLocationEnabled) {
+			runnable.run();
+			return true;
+		} else {
+			mRunOnFirstFix.addLast(runnable);
+			return false;
+		}
+	}
+	
+	private LocationManager getLocationManager() {
+		if(this.mLocationManager == null)
+			this.mLocationManager = (LocationManager) mCtx.getSystemService(Context.LOCATION_SERVICE);
+		return this.mLocationManager; 
+	}
+	
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
