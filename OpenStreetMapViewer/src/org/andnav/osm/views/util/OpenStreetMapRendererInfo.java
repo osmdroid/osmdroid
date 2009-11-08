eb7de4e0e56a7285d7cebd92297a553fdddd9a55
ThyMythos <ThyMythos@349629ee-b03d-11dd-8830-69e4bb380a4a>
2009-11-08 10:55:34 +0000

    Issue #12: add support for quadtree based server URLs

--- OpenStreetMapViewer/src/org/andnav/osm/views/util/OpenStreetMapRendererInfo.java
+++ OpenStreetMapViewer/src/org/andnav/osm/views/util/OpenStreetMapRendererInfo.java
@@ -5,32 +5,37 @@ import org.andnav.osm.R;
 import org.andnav.osm.services.util.OpenStreetMapTile;
 
 /**
- * 
+ * The OpenStreetMapRendererInfo stores information about available tile servers.
  * @author Nicolas Gramlich
  *
  */
 public enum OpenStreetMapRendererInfo {
-	OSMARENDER("http://tah.openstreetmap.org/Tiles/tile/", R.string.osmarender, ".png", 0, 17, 8),
-	MAPNIK("http://tile.openstreetmap.org/", R.string.mapnik, ".png", 0, 18, 8),
-	CYCLEMAP("http://b.andy.sandbox.cloudmade.com/tiles/cycle/", R.string.cyclemap, ".png", 0, 17, 8),
-	OPENARIELMAP("http://tile.openaerialmap.org/tiles/1.0.0/openaerialmap-900913/", R.string.openareal_sat, ".jpg", 0, 13, 8),
-	TRAILS("http://topo.geofabrik.de/trails/", R.string.trails, ".png", 4, 17, 8),
-	RELIEF("http://topo.geofabrik.de/relief/", R.string.relief, ".png", 8, 17, 8),
-	CLOUDMADESMALLTILES("http://tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/2/64/", R.string.cloudmade_small, ".jpg", 0, 13, 6),
-	CLOUDMADESTANDARDTILES("http://tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/2/256/", R.string.cloudmade_standard, ".jpg", 0, 18, 8);
+	OSMARENDER("http://tah.openstreetmap.org/Tiles/tile/", R.string.osmarender, ".png", 0, 17, 8, CodeScheme.X_Y),
+	MAPNIK("http://tile.openstreetmap.org/", R.string.mapnik, ".png", 0, 18, 8, CodeScheme.X_Y),
+	CYCLEMAP("http://b.andy.sandbox.cloudmade.com/tiles/cycle/", R.string.cyclemap, ".png", 0, 17, 8, CodeScheme.X_Y),
+	OPENARIELMAP("http://tile.openaerialmap.org/tiles/1.0.0/openaerialmap-900913/", R.string.openareal_sat, ".jpg", 0, 13, 8, CodeScheme.X_Y),
+	TRAILS("http://topo.geofabrik.de/trails/", R.string.trails, ".png", 4, 17, 8, CodeScheme.X_Y),
+	RELIEF("http://topo.geofabrik.de/relief/", R.string.relief, ".png", 8, 17, 8, CodeScheme.X_Y),
+	CLOUDMADESMALLTILES("http://tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/2/64/", R.string.cloudmade_small, ".jpg", 0, 13, 6, CodeScheme.X_Y),
+	CLOUDMADESTANDARDTILES("http://tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/2/256/", R.string.cloudmade_standard, ".jpg", 0, 18, 8, CodeScheme.X_Y);
 	
 	// ===========================================================
 	// Fields
 	// ===========================================================
 	
+	public enum CodeScheme { X_Y, QUAD_TREE };
+	
 	public final String BASEURL, IMAGE_FILENAMEENDING;
 	public final int NAME, ZOOM_MINLEVEL, ZOOM_MAXLEVEL, MAPTILE_ZOOM, MAPTILE_SIZEPX;
+	public final CodeScheme CODE_SCHEME;
 	
 	// ===========================================================
 	// Constructors
 	// ===========================================================
 	
-	private OpenStreetMapRendererInfo(final String aBaseUrl, final int aName, final String aImageFilenameEnding, final int aZoomMin, final int aZoomMax, final int aTileZoom){
+	private OpenStreetMapRendererInfo(final String aBaseUrl, final int aName,
+			final String aImageFilenameEnding, final int aZoomMin,
+			final int aZoomMax, final int aTileZoom, final CodeScheme aCodeScheme) {
 		this.BASEURL = aBaseUrl;
 		this.NAME = aName;
 		this.ZOOM_MINLEVEL = aZoomMin;
@@ -38,6 +43,7 @@ public enum OpenStreetMapRendererInfo {
 		this.IMAGE_FILENAMEENDING = aImageFilenameEnding;
 		this.MAPTILE_ZOOM = aTileZoom;
 		this.MAPTILE_SIZEPX = 1<<aTileZoom;
+		this.CODE_SCHEME = aCodeScheme;
 	}
 	
 	public static OpenStreetMapRendererInfo getDefault() {
@@ -49,6 +55,34 @@ public enum OpenStreetMapRendererInfo {
 	// ===========================================================
 	
 	public String getTileURLString(final OpenStreetMapTile aTile) {
-		return String.format("%s%d/%d/%d%s", this.BASEURL, aTile.zoomLevel, aTile.x, aTile.y, this.IMAGE_FILENAMEENDING);
+		final CodeScheme cs = this.CODE_SCHEME;
+		switch (cs) {
+		case QUAD_TREE:
+			return String.format("%s%s%s", this.BASEURL, quadTree(aTile), this.IMAGE_FILENAMEENDING);
+		case X_Y:
+		default:
+			return String.format("%s%d/%d/%d%s", this.BASEURL, aTile.zoomLevel, aTile.x, aTile.y, this.IMAGE_FILENAMEENDING);
+		}		
 	}
+	
+	/**
+	 * Converts TMS tile coordinates to QuadTree
+	 * @param aTile The tile coordinates to convert
+	 * @return The QuadTree as String.
+	 */
+	private String quadTree(final OpenStreetMapTile aTile) {
+		StringBuilder quadKey = new StringBuilder();
+		for (int i = aTile.zoomLevel; i > 0; i--) {
+			int digit = 0;
+			int mask = 1 << (i - 1);
+			if ((aTile.x & mask) != 0)
+				digit += 1;
+			if ((aTile.y & mask) != 0)
+				digit += 2;
+			quadKey.append("" + digit);
+		}
+
+		return quadKey.toString();
+	}
+	
 }
