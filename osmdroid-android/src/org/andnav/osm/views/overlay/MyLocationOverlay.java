09b589fb494bd141329c7e478030a6bf0195f5d2
neilboyd <neilboyd@349629ee-b03d-11dd-8830-69e4bb380a4a>
2010-08-16 08:07:12 +0000

    Make MyLocationOverlay request GPS update intervals customizable. Fixes issue 76.

--- osmdroid-android/src/org/andnav/osm/views/overlay/MyLocationOverlay.java
+++ osmdroid-android/src/org/andnav/osm/views/overlay/MyLocationOverlay.java
@@ -28,7 +28,7 @@ import android.util.Log;
 import android.view.MotionEvent;
 
 /**
- * 
+ *
  * @author Manuel Stahl
  *
  */
@@ -43,26 +43,28 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 	// ===========================================================
 	// Fields
 	// ===========================================================
-	
+
 	protected final Paint mPaint = new Paint();
 	protected final Paint mCirclePaint = new Paint();
-	
+
 	protected final Bitmap PERSON_ICON;
 	protected final Bitmap DIRECTION_ARROW;
-	
+
 	private final OpenStreetMapView mMapView;
 	private final OpenStreetMapViewController mMapController;
 	private final LocationManager mLocationManager;
 	private boolean mMyLocationEnabled = false;
 	private LinkedList<Runnable> mRunOnFirstFix = new LinkedList<Runnable>();
 	private final Point mMapCoords = new Point();
-	
+
 	private Location mLocation;
+	private long mLocationUpdateMinTime = 0;
+	private float mLocationUpdateMinDistance = 0.0f;
 	protected boolean mFollow = false;	// follow location updates
 	private NetworkLocationIgnorer mIgnorer = new NetworkLocationIgnorer();
-	
+
 	private final Matrix directionRotater = new Matrix();
-	
+
 	/** Coordinates the feet of the person are located. */
 	protected final android.graphics.Point PERSON_HOTSPOT = new android.graphics.Point(24,39);
 
@@ -72,7 +74,7 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 	// ===========================================================
 	// Constructors
 	// ===========================================================
-	
+
 	public MyLocationOverlay(final Context ctx, final OpenStreetMapView mapView) {
 		this(ctx, mapView, new DefaultResourceProxyImpl(ctx));
 	}
@@ -95,11 +97,11 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 	// ===========================================================
 	// Getter & Setter
 	// ===========================================================
-	
+
 	public Location getLastFix() {
 		return mLocation;
 	}
-	
+
 	/**
 	 * Return a GeoPoint of the last known location, or null if not known.
 	 */
@@ -114,40 +116,68 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 	public boolean isMyLocationEnabled() {
 		return mMyLocationEnabled;
 	}
-	
+
 	public boolean isLocationFollowEnabled() {
 		return mFollow;
 	}
-	
+
 	public void followLocation(boolean enable) {
 		mFollow = enable;
 	}
-	
+
+	public long getLocationUpdateMinTime() {
+		return mLocationUpdateMinTime;
+	}
+
+	/**
+	 * Set the minimum interval for location updates.
+	 * See {@link LocationManager.requestLocationUpdates(String, long, float, LocationListener)}.
+	 * Note that you should call this before calling {@link enableMyLocation()}.
+	 * @param milliSeconds
+	 */
+	public void setLocationUpdateMinTime(final long milliSeconds) {
+		mLocationUpdateMinTime = milliSeconds;
+	}
+
+	public float getLocationUpdateMinDistance() {
+		return mLocationUpdateMinDistance;
+	}
+
+	/**
+	 * Set the minimum distance for location updates.
+	 * See {@link LocationManager.requestLocationUpdates}.
+	 * Note that you should call this before calling {@link enableMyLocation()}.
+	 * @param meters
+	 */
+	public void setLocationUpdateMinDistance(final float meters) {
+		mLocationUpdateMinDistance = meters;
+	}
+
 	// ===========================================================
 	// Methods from SuperClass/Interfaces
 	// ===========================================================
-	
+
 	@Override
 	protected void onDrawFinished(Canvas c, OpenStreetMapView osmv) {}
-	
+
 	@Override
 	public void onDraw(final Canvas c, final OpenStreetMapView osmv) {
 		if(this.mLocation != null) {
 			final OpenStreetMapViewProjection pj = osmv.getProjection();
 			pj.toMapPixels(new GeoPoint(mLocation), mMapCoords);
 			final float radius = pj.metersToEquatorPixels(this.mLocation.getAccuracy());
-			
+
 			this.mCirclePaint.setAlpha(50);
 			this.mCirclePaint.setStyle(Style.FILL);
 			c.drawCircle(mMapCoords.x, mMapCoords.y, radius, this.mCirclePaint);
-			
+
 			this.mCirclePaint.setAlpha(150);
 			this.mCirclePaint.setStyle(Style.STROKE);
 			c.drawCircle(mMapCoords.x, mMapCoords.y, radius, this.mCirclePaint);
-			
+
 			float[] mtx = new float[9];
 			c.getMatrix().getValues(mtx);
-			
+
 			if (DEBUGMODE) {
 				float tx = (-mtx[Matrix.MTRANS_X]+20)/mtx[Matrix.MSCALE_X];
 				float ty = (-mtx[Matrix.MTRANS_Y]+90)/mtx[Matrix.MSCALE_Y];
@@ -156,11 +186,11 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 				c.drawText("Alt: " + mLocation.getAltitude(),  tx, ty + 35, this.mPaint);
 				c.drawText("Acc: " + mLocation.getAccuracy(),  tx, ty + 50, this.mPaint);
 			}
-			
+
 			if (mLocation.hasSpeed() && mLocation.getSpeed() > 1) {
 				/* Rotate the direction-Arrow according to the bearing we are driving. And draw it to the canvas. */
 				this.directionRotater.setRotate(this.mLocation.getBearing(), DIRECTION_ARROW_CENTER_X , DIRECTION_ARROW_CENTER_Y);
-				this.directionRotater.postTranslate(-DIRECTION_ARROW_CENTER_X, -DIRECTION_ARROW_CENTER_Y);			
+				this.directionRotater.postTranslate(-DIRECTION_ARROW_CENTER_X, -DIRECTION_ARROW_CENTER_Y);
 				this.directionRotater.postScale(1/mtx[Matrix.MSCALE_X], 1/mtx[Matrix.MSCALE_Y]);
 				this.directionRotater.postTranslate(mMapCoords.x, mMapCoords.y);
 				c.drawBitmap(DIRECTION_ARROW, this.directionRotater, this.mPaint);
@@ -192,15 +222,15 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 			mMapView.invalidate(); // redraw the my location icon
 		}
 	}
-	
+
 	@Override
 	public void onProviderDisabled(String provider) {
 	}
-	
+
 	@Override
 	public void onProviderEnabled(String provider) {
 	}
-	
+
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		if(status == LocationProvider.AVAILABLE) {
@@ -216,7 +246,7 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 			t.run();
 		}
 	}
-	
+
 	@Override
 	public boolean onSnapToItem(int x, int y, Point snapPoint, OpenStreetMapView mapView) {
 		if(this.mLocation != null) {
@@ -224,14 +254,14 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 			pj.toMapPixels(new GeoPoint(mLocation), mMapCoords);
 			snapPoint.x = mMapCoords.x;
 			snapPoint.y = mMapCoords.y;
-			
+
 			boolean snap = (x - mMapCoords.x)*(x - mMapCoords.x) + (y - mMapCoords.y)*(y - mMapCoords.y) < 64;
 			return snap;
 		} else {
 			return false;
 		}
 	}
-	
+
 	@Override
 	public boolean onTouchEvent(MotionEvent event, OpenStreetMapView mapView) {
 		if (event.getAction() == MotionEvent.ACTION_MOVE)
@@ -248,16 +278,23 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 		mLocationManager.removeUpdates(this);
 		mMyLocationEnabled = false;
 	}
-	
+
+	/**
+	 * Enable location updates so that the map follows the current location.
+	 * By default this will request location updates as frequently as possible,
+	 * but you can change the frequency and/or distance by calling
+	 * {@link setLocationUpdateMinTime(long)} and/or {@link setLocationUpdateMinDistance(float)}
+	 * before calling this method.
+	 */
 	public boolean enableMyLocation() {
 		if (!mMyLocationEnabled) {
 			for (final String provider : mLocationManager.getAllProviders()) {
-				mLocationManager.requestLocationUpdates(provider, 0, 0, this);
+				mLocationManager.requestLocationUpdates(provider, mLocationUpdateMinTime, mLocationUpdateMinDistance, this);
 			}
 		}
 		return mMyLocationEnabled = true;
 	}
-	
+
 	public boolean runOnFirstFix(Runnable runnable) {
 		if(mMyLocationEnabled) {
 			runnable.run();
@@ -267,7 +304,7 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 			return false;
 		}
 	}
-	
+
 	// ===========================================================
 	// Inner and Anonymous Classes
 	// ===========================================================
