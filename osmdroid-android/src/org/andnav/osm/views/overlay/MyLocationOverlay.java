478663445d67afdd1d0dbd89aeb4e374db320a90
neilboyd <neilboyd@349629ee-b03d-11dd-8830-69e4bb380a4a>
2010-08-17 08:14:21 +0000

    Add compass to MyLocationOverlay. See issue 77. Not tested, but seems to be fairly unobtrusive.

--- osmdroid-android/src/org/andnav/osm/views/overlay/MyLocationOverlay.java
+++ osmdroid-android/src/org/andnav/osm/views/overlay/MyLocationOverlay.java
@@ -15,10 +15,18 @@ import org.andnav.osm.views.overlay.OpenStreetMapViewOverlay.Snappable;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.graphics.Path;
+import android.graphics.Picture;
 import android.graphics.Point;
+import android.graphics.Rect;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -32,7 +40,7 @@ import android.view.MotionEvent;
  * @author Manuel Stahl
  *
  */
-public class MyLocationOverlay extends OpenStreetMapViewOverlay implements LocationListener, Snappable {
+public class MyLocationOverlay extends OpenStreetMapViewOverlay implements SensorEventListener, LocationListener, Snappable {
 
 	public static final String DEBUGTAG = "OPENSTREETMAP";
 
@@ -53,6 +61,8 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 	private final OpenStreetMapView mMapView;
 	private final OpenStreetMapViewController mMapController;
 	private final LocationManager mLocationManager;
+	private final SensorManager mSensorManager;
+
 	private boolean mMyLocationEnabled = false;
 	private LinkedList<Runnable> mRunOnFirstFix = new LinkedList<Runnable>();
 	private final Point mMapCoords = new Point();
@@ -71,6 +81,20 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 	private final float DIRECTION_ARROW_CENTER_X;
 	private final float DIRECTION_ARROW_CENTER_Y;
 
+	// Compass values
+	private boolean mCompassEnabled = false;
+
+	protected final Picture mCompassFrame = new Picture();
+	protected final Picture mCompassRose = new Picture();
+
+	private float mAzimuth = 0.0f;
+
+	private float mCompassCenterX = 35.0f;
+	private float mCompassCenterY = 35.0f;
+	private float mCompassRadius = 20.0f;
+
+	private float mScale = 1.0f;
+
 	// ===========================================================
 	// Constructors
 	// ===========================================================
@@ -83,6 +107,7 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 		super(pResourceProxy);
 		mMapView = mapView;
 		mLocationManager = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
+		mSensorManager = (SensorManager) ctx.getSystemService(Context.SENSOR_SERVICE);
 		mMapController = mapView.getController();
 		mCirclePaint.setARGB(0, 100, 100, 255);
 		mCirclePaint.setAntiAlias(true);
@@ -92,6 +117,11 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 
 		DIRECTION_ARROW_CENTER_X = DIRECTION_ARROW.getWidth() / 2 - 0.5f;
 		DIRECTION_ARROW_CENTER_Y = DIRECTION_ARROW.getHeight() / 2 - 0.5f;
+
+		mScale = ctx.getResources().getDisplayMetrics().density;
+
+		createCompassFramePicture();
+		createCompassRosePicture();
 	}
 
 	// ===========================================================
@@ -115,6 +145,10 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 
 	public boolean isMyLocationEnabled() {
 		return mMyLocationEnabled;
+	}
+
+	public boolean isCompassEnabled() {
+		return mCompassEnabled;
 	}
 
 	public boolean isLocationFollowEnabled() {
@@ -151,6 +185,11 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 	 */
 	public void setLocationUpdateMinDistance(final float meters) {
 		mLocationUpdateMinDistance = meters;
+	}
+
+	public void setCompassCenter(float x, float y) {
+		mCompassCenterX = x;
+		mCompassCenterY = y;
 	}
 
 	// ===========================================================
@@ -201,6 +240,31 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 				c.drawBitmap(PERSON_ICON, this.directionRotater, this.mPaint);
 			}
 		}
+
+		if (mCompassEnabled) { // && (mAzimuth >= 0.0f)
+
+			float[] matrix = new float[9];
+			c.getMatrix().getValues(matrix);
+
+			final float tx = (-matrix[Matrix.MTRANS_X])/matrix[Matrix.MSCALE_X];
+			final float ty = (-matrix[Matrix.MTRANS_Y])/matrix[Matrix.MSCALE_Y];
+
+			final float centerX = tx + (mCompassCenterX * mScale);
+			final float centerY = ty + (mCompassCenterY * mScale) + (c.getHeight() - mMapView.getHeight());
+
+			final int left = (int) centerX - mCompassFrame.getWidth() / 2;
+			final int top = (int) centerY - mCompassFrame.getHeight() / 2;
+
+			final Rect drawingTarget = new Rect(left, top, left + mCompassFrame.getWidth(), top + mCompassFrame.getHeight());
+
+			c.drawPicture(mCompassFrame, drawingTarget);
+
+			c.save();
+			c.rotate(-mAzimuth, centerX, centerY);
+			c.drawPicture(mCompassRose, drawingTarget);
+			c.restore();
+		}
+
 	}
 
 	@Override
@@ -270,6 +334,23 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 		return super.onTouchEvent(event, mapView);
 	}
 
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if ((mCompassEnabled) && (event.sensor.getType() == Sensor.TYPE_ORIENTATION))
+		{
+			if (event.values != null)
+			{
+				mAzimuth = (float) event.values[0];
+				mMapView.invalidate();
+			}
+		}
+	}
+
 	// ===========================================================
 	// Methods
 	// ===========================================================
@@ -295,6 +376,19 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 		return mMyLocationEnabled = true;
 	}
 
+	public void enableCompass() {
+		if (!mCompassEnabled) {
+			final Sensor sensorOrientation = this.mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+			mSensorManager.registerListener(this, sensorOrientation, SensorManager.SENSOR_DELAY_UI);
+		}
+		mCompassEnabled = true;
+	}
+
+	public void disableCompass() {
+		mSensorManager.unregisterListener(this);
+		mCompassEnabled = false;
+	}
+
 	public boolean runOnFirstFix(Runnable runnable) {
 		if(mMyLocationEnabled) {
 			runnable.run();
@@ -308,4 +402,104 @@ public class MyLocationOverlay extends OpenStreetMapViewOverlay implements Locat
 	// ===========================================================
 	// Inner and Anonymous Classes
 	// ===========================================================
+
+	private Point calculatePointOnCircle(float centerX, float centerY, float radius, float degrees) {
+		// for trigonometry, 0 is pointing east, so subtract 90
+		// compass degrees are the wrong way round
+		final double dblRadians = Math.toRadians(-degrees + 90);
+
+		final int intX = (int) (radius * Math.cos(dblRadians));
+		final int intY = (int) (radius * Math.sin(dblRadians));
+
+		return new Point((int) centerX + intX, (int) centerY - intY);
+	}
+
+	private void drawTriangle(Canvas canvas, float x, float y, float radius, float degrees, Paint paint) {
+		canvas.save();
+		final Point point = this.calculatePointOnCircle(x, y, radius, degrees);
+		canvas.rotate(degrees, point.x, point.y);
+		final Path p = new Path();
+		p.moveTo(point.x - 2 * mScale, point.y);
+		p.lineTo(point.x + 2 * mScale, point.y);
+		p.lineTo(point.x, point.y - 5 * mScale);
+		p.close();
+		canvas.drawPath(p, paint);
+		canvas.restore();
+	}
+
+	private void createCompassFramePicture() {
+		// The inside of the compass is white and transparent
+		final Paint innerPaint = new Paint();
+		innerPaint.setColor(Color.WHITE);
+		innerPaint.setAntiAlias(true);
+		innerPaint.setStyle(Style.FILL);
+		innerPaint.setAlpha(200);
+
+		// The outer part (circle and little triangles) is gray and transparent
+		final Paint outerPaint = new Paint();
+		outerPaint.setColor(Color.GRAY);
+		outerPaint.setAntiAlias(true);
+		outerPaint.setStyle(Style.STROKE);
+		outerPaint.setStrokeWidth(2.0f);
+		outerPaint.setAlpha(200);
+
+		final int picBorderWidthAndHeight = (int) ((mCompassRadius + 5) * 2 * mScale);
+		final int center = picBorderWidthAndHeight / 2;
+
+		final Canvas canvas = mCompassFrame.beginRecording(picBorderWidthAndHeight, picBorderWidthAndHeight);
+
+		// draw compass inner circle and border
+		canvas.drawCircle(center, center, mCompassRadius * mScale, innerPaint);
+		canvas.drawCircle(center, center, mCompassRadius * mScale, outerPaint);
+
+		// Draw little triangles north, south, west and east (don't move)
+		// to make those move use "-bearing + 0" etc. (Note: that would mean to draw the triangles in the onDraw() method)
+		drawTriangle(canvas, center, center, mCompassRadius * mScale, 0, outerPaint);
+		drawTriangle(canvas, center, center, mCompassRadius * mScale, 90, outerPaint);
+		drawTriangle(canvas, center, center, mCompassRadius * mScale, 180, outerPaint);
+		drawTriangle(canvas, center, center, mCompassRadius * mScale, 270, outerPaint);
+
+		mCompassFrame.endRecording();
+	}
+
+	private void createCompassRosePicture() {
+		// Paint design of north triangle (it's common to paint north in blue color)
+		final Paint northPaint = new Paint();
+		northPaint.setColor(0xFF0000A0);
+		northPaint.setAntiAlias(true);
+		northPaint.setStyle(Style.FILL);
+		northPaint.setAlpha(220);
+
+		// Paint design of south triangle (red)
+		final Paint southPaint = new Paint();
+		southPaint.setColor(0xFFA00000);
+		southPaint.setAntiAlias(true);
+		southPaint.setStyle(Style.FILL);
+		southPaint.setAlpha(220);
+
+		final int picBorderWidthAndHeight = (int) ((mCompassRadius + 5) * 2 * mScale);
+		final int center = picBorderWidthAndHeight / 2;
+
+		final Canvas canvas = mCompassRose.beginRecording(picBorderWidthAndHeight, picBorderWidthAndHeight);
+
+		// Blue triangle pointing north
+		final Path pathNorth = new Path();
+		pathNorth.moveTo(center, center - (mCompassRadius - 3) * mScale);
+		pathNorth.lineTo(center + 4 * mScale, center);
+		pathNorth.lineTo(center - 4 * mScale, center);
+		pathNorth.lineTo(center, center - (mCompassRadius - 3) * mScale);
+		pathNorth.close();
+		canvas.drawPath(pathNorth, northPaint);
+
+		// Red triangle pointing south
+		final Path pathSouth = new Path();
+		pathSouth.moveTo(center, center + (mCompassRadius - 3) * mScale);
+		pathSouth.lineTo(center + 4 * mScale, center);
+		pathSouth.lineTo(center - 4 * mScale, center);
+		pathSouth.lineTo(center, center + (mCompassRadius - 3) * mScale);
+		pathSouth.close();
+		canvas.drawPath(pathSouth, southPaint);
+
+		mCompassRose.endRecording();
+	}
 }
