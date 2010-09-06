ac2637681670d12e7638ba72e9be515a8c7d0f23
neilboyd <neilboyd@349629ee-b03d-11dd-8830-69e4bb380a4a>
2010-09-06 07:27:20 +0000

    Typo in SampleWithMinimapZoomcontrols. java http://groups.google.com/group/osmdroid/browse_thread/thread/859705ec65ebf292/f4351bac8019d1d2

--- OpenStreetMapViewer/src/org/andnav/osm/samples/SampleWithMinimapZoomcontrols.java
+++ OpenStreetMapViewer/src/org/andnav/osm/samples/SampleWithMinimapZoomcontrols.java
@@ -14,12 +14,12 @@ import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 
 /**
- * 
+ *
  * @author Nicolas Gramlich
  *
  */
 public class SampleWithMinimapZoomcontrols extends Activity {
-	
+
 	// ===========================================================
 	// Constants
 	// ===========================================================
@@ -27,8 +27,8 @@ public class SampleWithMinimapZoomcontrols extends Activity {
 	// ===========================================================
 	// Fields
 	// ===========================================================
-	
-	private OpenStreetMapView mOsmv, mOsmvMinimap; 
+
+	private OpenStreetMapView mOsmv, mOsmvMinimap;
 
 	// ===========================================================
 	// Constructors
@@ -37,13 +37,13 @@ public class SampleWithMinimapZoomcontrols extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         final RelativeLayout rl = new RelativeLayout(this);
-        
+
         this.mOsmv = new OpenStreetMapView(this);
         rl.addView(this.mOsmv, new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
-        
-        
+
+
         /* ZoomControls */
         {
 	        /* Create a ImageView with a zoomIn-Icon. */
@@ -54,25 +54,25 @@ public class SampleWithMinimapZoomcontrols extends Activity {
 	        zoominParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
 	        zoominParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
 	        rl.addView(ivZoomIn, zoominParams);
-	        
+
 	        ivZoomIn.setOnClickListener(new OnClickListener(){
 				@Override
 				public void onClick(View v) {
 					SampleWithMinimapZoomcontrols.this.mOsmv.getController().zoomIn();
 				}
 	        });
-	        
-	        
+
+
 	        /* Create a ImageView with a zoomOut-Icon. */
 	        final ImageView ivZoomOut = new ImageView(this);
-	        ivZoomIn.setImageResource(R.drawable.zoom_out);
-	        
+	        ivZoomOut.setImageResource(R.drawable.zoom_out);
+
 	        /* Create RelativeLayoutParams, that position in in the top left corner. */
 	        final RelativeLayout.LayoutParams zoomoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
 	        zoomoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
 	        zoomoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
 	        rl.addView(ivZoomOut, zoomoutParams);
-	        
+
 	        ivZoomOut.setOnClickListener(new OnClickListener(){
 				@Override
 				public void onClick(View v) {
@@ -80,15 +80,15 @@ public class SampleWithMinimapZoomcontrols extends Activity {
 				}
 	        });
         }
-        
+
         /* MiniMap */
         {
 	        /* Create another OpenStreetMapView, that will act as the MiniMap for the 'MainMap'. They will share the TileProvider. */
 	        mOsmvMinimap = new OpenStreetMapView(this, OpenStreetMapRendererInfo.CLOUDMADESTANDARDTILES, this.mOsmv);
 	        final int aZoomDiff = 3; // Use OpenStreetMapViewConstants.NOT_SET to disable autozooming of this minimap
 	        this.mOsmv.setMiniMap(mOsmvMinimap, aZoomDiff);
-	        
-	        
+
+
 	        /* Create RelativeLayout.LayoutParams that position the MiniMap on the top-right corner of the RelativeLayout. */
 	        RelativeLayout.LayoutParams minimapParams = new RelativeLayout.LayoutParams(90, 90);
 	        minimapParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
@@ -96,10 +96,10 @@ public class SampleWithMinimapZoomcontrols extends Activity {
 	        minimapParams.setMargins(5,5,5,5);
 	        rl.addView(mOsmvMinimap, minimapParams);
         }
-        
+
         this.setContentView(rl);
     }
- 
+
 	// ===========================================================
 	// Getter & Setter
 	// ===========================================================
