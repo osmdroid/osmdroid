d0998cea5768fefddcaab20d9f8b637d81f6ec40
kurtzmarc <kurtzmarc@349629ee-b03d-11dd-8830-69e4bb380a4a>
2011-01-01 03:51:06 +0000

    - Copy from trunk, merge from branch

---
+++ osmdroid-android/src/org/osmdroid/ResourceProxy.java
@@ -0,0 +1,72 @@
+package org.andnav.osm;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+
+public interface ResourceProxy {
+
+	public static enum string {
+
+		// tile sources
+		osmarender,
+		mapnik,
+		cyclemap,
+		public_transport,
+		base,
+		topo,
+		hills,
+		cloudmade_small,
+		cloudmade_standard,
+
+		// overlays
+		fiets_nl,
+		base_nl,
+		roads_nl,
+
+		// other stuff
+		unknown,
+		format_distance_meters,
+		format_distance_kilometers,
+		format_distance_miles,
+		format_distance_nautical_miles,
+		format_distance_feet,
+
+	}
+
+	public static enum bitmap {
+
+		/**
+		 * For testing - the image doesn't exist.
+		 */
+		unknown,
+
+		center,
+		direction_arrow,
+		marker_default,
+		marker_default_focused_base,
+		navto_small,
+		next,
+		previous,
+		person,
+
+	}
+
+	String getString(string pResId);
+
+	/**
+	 * Use a string resource as a format definition, and format using the supplied format arguments.
+	 * @param pResId
+	 * @param formatArgs
+	 * @return
+	 */
+	String getString(string pResId, Object... formatArgs);
+
+	Bitmap getBitmap(bitmap pResId);
+
+	/**
+	 * Get a bitmap as a {@link Drawable}
+	 * @param pResId
+	 * @return
+	 */
+	Drawable getDrawable(bitmap pResId);
+}
