d0619e66f18d33deb3677cca53c3b8f9fe937ccb
neilboyd <neilboyd@349629ee-b03d-11dd-8830-69e4bb380a4a>
2011-01-04 10:16:06 +0000

    formatting

--- osmdroid-android/src/org/osmdroid/tileprovider/modules/MapTileModuleProviderBase.java
+++ osmdroid-android/src/org/osmdroid/tileprovider/modules/MapTileModuleProviderBase.java
@@ -17,23 +17,22 @@ import android.graphics.drawable.Drawable;
 
 /**
  * An abstract base class for modular tile providers
- * 
+ *
  * @author Marc Kurtz
  * @author Neil Boyd
  */
-public abstract class MapTileModuleProviderBase implements
-		OpenStreetMapTileProviderConstants {
+public abstract class MapTileModuleProviderBase implements OpenStreetMapTileProviderConstants {
 
 	/**
 	 * Gets the human-friendly name assigned to this tile provider.
-	 * 
+	 *
 	 * @return the thread name
 	 */
 	protected abstract String getName();
 
 	/**
 	 * Gets the name assigned to the thread for this provider.
-	 * 
+	 *
 	 * @return the thread name
 	 */
 	protected abstract String getThreadGroupName();
@@ -42,7 +41,7 @@ public abstract class MapTileModuleProviderBase implements
 	 * It is expected that the implementation will construct an internal member which internally
 	 * implements a {@link TileLoader}. This method is expected to return a that internal member to
 	 * methods of the parent methods.
-	 * 
+	 *
 	 * @return the internal member of this tile provider.
 	 */
 	protected abstract Runnable getTileLoader();
@@ -50,63 +49,61 @@ public abstract class MapTileModuleProviderBase implements
 	/**
 	 * Returns true if implementation uses a data connection, false otherwise. This value is used to
 	 * determine if this provider should be skipped if there is no data connection.
-	 * 
+	 *
 	 * @return true if implementation uses a data connection, false otherwise
 	 */
 	public abstract boolean getUsesDataConnection();
 
 	/**
 	 * Gets the minimum zoom level this tile provider can provide
-	 * 
+	 *
 	 * @return the minimum zoom level
 	 */
 	public abstract int getMinimumZoomLevel();
 
 	/**
 	 * Gets the maximum zoom level this tile provider can provide
-	 * 
+	 *
 	 * @return the maximum zoom level
 	 */
 	public abstract int getMaximumZoomLevel();
 
 	/**
 	 * Sets the tile source for this tile provider.
-	 * 
+	 *
 	 * @param tileSource
 	 *            the tile source
 	 */
 	public abstract void setTileSource(ITileSource tileSource);
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(MapTileModuleProviderBase.class);
+	private static final Logger logger = LoggerFactory.getLogger(MapTileModuleProviderBase.class);
 
 	private final int mThreadPoolSize;
 	private final ThreadGroup mThreadPool = new ThreadGroup(getThreadGroupName());
 	private final ConcurrentHashMap<MapTile, MapTileRequestState> mWorking;
 	final LinkedHashMap<MapTile, MapTileRequestState> mPending;
 
-	public MapTileModuleProviderBase(final int aThreadPoolSize,
-			final int aPendingQueueSize) {
-		mThreadPoolSize = aThreadPoolSize;
+	public MapTileModuleProviderBase(final int pThreadPoolSize,	final int pPendingQueueSize) {
+		mThreadPoolSize = pThreadPoolSize;
 		mWorking = new ConcurrentHashMap<MapTile, MapTileRequestState>();
-		mPending = new LinkedHashMap<MapTile, MapTileRequestState>(
-				aPendingQueueSize + 2, 0.1f, true) {
+		mPending = new LinkedHashMap<MapTile, MapTileRequestState>(pPendingQueueSize + 2, 0.1f, true) {
 			private static final long serialVersionUID = 6455337315681858866L;
 		};
 	}
 
-	public void loadMapTileAsync(final MapTileRequestState aState) {
+	public void loadMapTileAsync(final MapTileRequestState pState) {
 
 		final int activeCount = mThreadPool.activeCount();
 
 		synchronized (mPending) {
 			// this will put the tile in the queue, or move it to the front of
 			// the queue if it's already present
-			mPending.put(aState.getMapTile(), aState);
+			mPending.put(pState.getMapTile(), pState);
 		}
 
-		if (DEBUGMODE)
+		if (DEBUGMODE) {
 			logger.debug(activeCount + " active threads");
+		}
 		if (activeCount < mThreadPoolSize) {
 			final Thread t = new Thread(mThreadPool, getTileLoader());
 			t.start();
@@ -139,7 +136,7 @@ public abstract class MapTileModuleProviderBase implements
 	 * Load the requested tile. An abstract internal class whose objects are used by worker threads
 	 * to acquire tiles from servers. It processes tiles from the 'pending' set to the 'working' set
 	 * as they become available. The key unimplemented method is 'loadTile'.
-	 * 
+	 *
 	 * @param aTile
 	 *            the tile to load
 	 * @throws CantContinueException
@@ -149,14 +146,14 @@ public abstract class MapTileModuleProviderBase implements
 
 		/**
 		 * The key unimplemented method.
-		 * 
+		 *
 		 * @return true if the tile was loaded successfully and other tile providers need not be
 		 *         called, false otherwise
-		 * @param aTile
+		 * @param pState
 		 * @throws {@link CantContinueException}
 		 */
-		protected abstract Drawable loadTile(MapTileRequestState aState)
-				throws CantContinueException;
+		protected abstract Drawable loadTile(MapTileRequestState pState)
+		throws CantContinueException;
 
 		private MapTileRequestState nextTile() {
 
@@ -176,9 +173,10 @@ public abstract class MapTileModuleProviderBase implements
 							result = tile;
 						}
 					} catch (final ConcurrentModificationException e) {
-						if (DEBUGMODE)
+						if (DEBUGMODE) {
 							logger.warn("ConcurrentModificationException break: "
 									+ (result != null));
+						}
 
 						// if we've got a result return it, otherwise try again
 						if (result != null) {
@@ -199,27 +197,22 @@ public abstract class MapTileModuleProviderBase implements
 
 		/**
 		 * A tile has loaded.
-		 * 
-		 * @param aTile
-		 *            the tile that has loaded
-		 * @param aTileInputStream
-		 *            the input stream of the file.
 		 */
-		private void tileLoaded(final MapTileRequestState aState, final Drawable aDrawable) {
-			removeTileFromQueues(aState.getMapTile());
+		private void tileLoaded(final MapTileRequestState pState, final Drawable pDrawable) {
+			removeTileFromQueues(pState.getMapTile());
 
-			aState.getCallback().mapTileRequestCompleted(aState, aDrawable);
+			pState.getCallback().mapTileRequestCompleted(pState, pDrawable);
 		}
 
-		protected void tileCandidateLoaded(final MapTileRequestState aState,
-				final Drawable aDrawable) {
-			aState.getCallback().mapTileRequestCandidate(aState, aDrawable);
+		protected void tileCandidateLoaded(final MapTileRequestState pState,
+				final Drawable pDrawable) {
+			pState.getCallback().mapTileRequestCandidate(pState, pDrawable);
 		}
 
-		private void tileLoadedFailed(final MapTileRequestState aState) {
-			removeTileFromQueues(aState.getMapTile());
+		private void tileLoadedFailed(final MapTileRequestState pState) {
+			removeTileFromQueues(pState.getMapTile());
 
-			aState.getCallback().mapTileRequestFailed(aState);
+			pState.getCallback().mapTileRequestFailed(pState);
 		}
 
 		/**
@@ -231,8 +224,9 @@ public abstract class MapTileModuleProviderBase implements
 			MapTileRequestState state;
 			Drawable result = null;
 			while ((state = nextTile()) != null) {
-				if (DEBUGMODE)
+				if (DEBUGMODE) {
 					logger.debug("Next tile: " + state);
+				}
 				try {
 					result = null;
 					result = loadTile(state);
@@ -243,13 +237,15 @@ public abstract class MapTileModuleProviderBase implements
 					logger.error("Error downloading tile: " + state, e);
 				}
 
-				if (result != null)
+				if (result != null) {
 					tileLoaded(state, result);
-				else
+				} else {
 					tileLoadedFailed(state);
+				}
 
-				if (DEBUGMODE)
+				if (DEBUGMODE) {
 					logger.debug("No more tiles");
+				}
 			}
 		}
 	}
@@ -257,12 +253,12 @@ public abstract class MapTileModuleProviderBase implements
 	class CantContinueException extends Exception {
 		private static final long serialVersionUID = 146526524087765133L;
 
-		public CantContinueException(final String aDetailMessage) {
-			super(aDetailMessage);
+		public CantContinueException(final String pDetailMessage) {
+			super(pDetailMessage);
 		}
 
-		public CantContinueException(final Throwable aThrowable) {
-			super(aThrowable);
+		public CantContinueException(final Throwable pThrowable) {
+			super(pThrowable);
 		}
 	}
 }
