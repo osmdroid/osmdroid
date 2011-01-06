51ae73e25c6e53905cf6ed31c8378374e883dd9e
neilboyd <neilboyd@349629ee-b03d-11dd-8830-69e4bb380a4a>
2011-01-06 15:54:23 +0000

    Persist the Cloudmade key and token. Fixes issue 75.

--- osmdroid-android/src/org/osmdroid/tileprovider/tilesource/CloudmadeTileSource.java
+++ osmdroid-android/src/org/osmdroid/tileprovider/tilesource/CloudmadeTileSource.java
@@ -3,22 +3,16 @@ package org.osmdroid.tileprovider.tilesource;
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.tileprovider.MapTile;
 import org.osmdroid.tileprovider.util.CloudmadeUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 class CloudmadeTileSource extends OnlineTileSourceBase implements IStyledTileSource<Integer> {
 
-	private static final Logger logger = LoggerFactory.getLogger(CloudmadeTileSource.class);
-
-	private String mKey;
-	private String mToken;
 	private Integer mStyle = 1;
 
-	CloudmadeTileSource(final String aName, final ResourceProxy.string aResourceId,
-			final int aZoomMinLevel, final int aZoomMaxLevel, final int aTileSizePixels,
-			final String aImageFilenameEnding, final String... aBaseUrl) {
-		super(aName, aResourceId, aZoomMinLevel, aZoomMaxLevel, aTileSizePixels,
-				aImageFilenameEnding, aBaseUrl);
+	CloudmadeTileSource(final String pName, final ResourceProxy.string pResourceId,
+			final int pZoomMinLevel, final int pZoomMaxLevel, final int pTileSizePixels,
+			final String pImageFilenameEnding, final String... pBaseUrl) {
+		super(pName, pResourceId, pZoomMinLevel, pZoomMaxLevel, pTileSizePixels,
+				pImageFilenameEnding, pBaseUrl);
 	}
 
 	@Override
@@ -31,23 +25,11 @@ class CloudmadeTileSource extends OnlineTileSourceBase implements IStyledTileSou
 	}
 
 	@Override
-	public String getTileURLString(final MapTile aTile) {
-		if (mKey == null) {
-			mKey = CloudmadeUtil.getCloudmadeKey();
-		}
-		if (mToken == null) {
-			synchronized (mKey) {
-				if (mToken == null) {
-					try {
-						mToken = CloudmadeUtil.getCloudmadeToken(mKey);
-					} catch (final CloudmadeException e) {
-						return null;
-					}
-				}
-			}
-		}
-		return String.format(getBaseUrl(), mKey, mStyle, getTileSizePixels(), aTile.getZoomLevel(),
-				aTile.getX(), aTile.getY(), mImageFilenameEnding, mToken);
+	public String getTileURLString(final MapTile pTile) {
+		final String key = CloudmadeUtil.getCloudmadeKey();
+		final String token = CloudmadeUtil.getCloudmadeToken();
+		return String.format(getBaseUrl(), key, mStyle, getTileSizePixels(), pTile.getZoomLevel(),
+				pTile.getX(), pTile.getY(), mImageFilenameEnding, token);
 	}
 
 	@Override
