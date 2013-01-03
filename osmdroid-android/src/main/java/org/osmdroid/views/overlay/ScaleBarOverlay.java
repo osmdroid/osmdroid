f134ec4b120a2e1674dd7a9cc7ea7ced53be8e7e
kurtzmarc@gmail.com <kurtzmarc@gmail.com@349629ee-b03d-11dd-8830-69e4bb380a4a>
2013-01-03 18:22:31 +0000

    Fixes Issue 379. Adds various ScaleBar improvements.

--- osmdroid-android/src/main/java/org/osmdroid/views/overlay/ScaleBarOverlay.java
+++ osmdroid-android/src/main/java/org/osmdroid/views/overlay/ScaleBarOverlay.java
@@ -91,10 +91,15 @@ public class ScaleBarOverlay extends SafeDrawOverlay implements GeoConstants {
 
 	private final ResourceProxy resourceProxy;
 	private Paint barPaint;
+	private Paint bgPaint;
 	private Paint textPaint;
 	private Projection projection;
 
 	final private Rect mBounds = new Rect();
+
+	private boolean centred = false;
+	private boolean adjustLength = false;
+	private float maxLength;
 
 	// ===========================================================
 	// Constructors
@@ -114,6 +119,7 @@ public class ScaleBarOverlay extends SafeDrawOverlay implements GeoConstants {
 		this.barPaint.setAntiAlias(true);
 		this.barPaint.setStyle(Style.FILL);
 		this.barPaint.setAlpha(255);
+		this.bgPaint = null;
 
 		this.textPaint = new Paint();
 		this.textPaint.setColor(Color.BLACK);
@@ -155,75 +161,169 @@ public class ScaleBarOverlay extends SafeDrawOverlay implements GeoConstants {
 			this.ydpi = 264;
 		}
 
+		// set default max length to 1 inch
+		maxLength = 2.54f;
 	}
 
 	// ===========================================================
 	// Getter & Setter
 	// ===========================================================
 
+	/**
+	 * Sets the minimum zoom level for the scale bar to be drawn.
+	 * @param minimum zoom level
+	 */
 	public void setMinZoom(final int zoom) {
 		this.minZoom = zoom;
 	}
 
+	/**
+	 * Sets the scale bar screen offset for the bar. Note: if the bar is set to be drawn centered, this will be the middle of the bar, otherwise the top left corner.   
+	 * @param x x screen offset
+	 * @param y z screen offset
+	 */
 	public void setScaleBarOffset(final float x, final float y) {
 		xOffset = x;
 		yOffset = y;
 	}
 
+	/**
+	 * Sets the bar's line width. (the default is 2)
+	 * @param width the new line width
+	 */
 	public void setLineWidth(final float width) {
 		this.lineWidth = width;
 	}
 
+	/**
+	 * Sets the text size. (the default is 12)
+	 * @param size the new text size
+	 */
 	public void setTextSize(final float size) {
 		this.textPaint.setTextSize(size);
 	}
 
+	/**
+	 * Sets the length to be shown in imperial units (mi/ft)
+	 */
 	public void setImperial() {
 		this.imperial = true;
 		this.nautical = false;
 		lastZoomLevel = -1; // Force redraw of scalebar
 	}
 
+	/**
+	 * Sets the length to be shown in nautical units (nm/ft)
+	 */
 	public void setNautical() {
 		this.nautical = true;
 		this.imperial = false;
 		lastZoomLevel = -1; // Force redraw of scalebar
 	}
 
+	/**
+	 * Sets the length to be shown in metric units (km/m)
+	 */
 	public void setMetric() {
 		this.nautical = false;
 		this.imperial = false;
 		lastZoomLevel = -1; // Force redraw of scalebar
 	}
 
+	/**
+	 * Latitudinal / horizontal scale bar flag
+	 * @param latitude
+	 */
 	public void drawLatitudeScale(final boolean latitude) {
 		this.latitudeBar = latitude;
+		lastZoomLevel = -1; // Force redraw of scalebar
 	}
 
+	/**
+	 * Longitudinal / vertical scale bar flag
+	 * @param longitude
+	 */
 	public void drawLongitudeScale(final boolean longitude) {
 		this.longitudeBar = longitude;
+		lastZoomLevel = -1; // Force redraw of scalebar
 	}
 
+	/**
+	 * Flag to draw the bar centered around the set offset coordinates or to the right/bottom of the coordinates (default)
+	 * @param centred set true to centre the bar around the given screen coordinates
+	 */
+	public void setCentred(final boolean centred) {
+		this.centred = centred;
+		lastZoomLevel = -1; // Force redraw of scalebar
+	}
+
+	/**
+	 * Return's the paint used to draw the bar
+	 * @return the paint used to draw the bar
+	 */
 	public Paint getBarPaint() {
 		return barPaint;
 	}
 
+	/**
+	 * Sets the paint for drawing the bar
+	 * @param pBarPaint bar drawing paint
+	 */
 	public void setBarPaint(final Paint pBarPaint) {
 		if (pBarPaint == null) {
 			throw new IllegalArgumentException("pBarPaint argument cannot be null");
 		}
 		barPaint = pBarPaint;
+		lastZoomLevel = -1; // Force redraw of scalebar
 	}
 
+	/**
+	 * Returns the paint used to draw the text
+	 * @return the paint used to draw the text
+	 */
 	public Paint getTextPaint() {
 		return textPaint;
 	}
 
+	/**
+	 * Sets the paint for drawing the text
+	 * @param pTextPaint text drawing paint
+	 */
 	public void setTextPaint(final Paint pTextPaint) {
 		if (pTextPaint == null) {
 			throw new IllegalArgumentException("pTextPaint argument cannot be null");
 		}
 		textPaint = pTextPaint;
+		lastZoomLevel = -1; // Force redraw of scalebar
+	}
+
+	/**
+	 * Sets the background paint. Set to null to disable drawing of background (default)
+	 * @param pBgPaint the paint for colouring the bar background
+	 */
+	public void setBackgroundPaint(final Paint pBgPaint) {
+		bgPaint = pBgPaint;
+		lastZoomLevel = -1; // Force redraw of scalebar
+	}
+
+	/**
+	 * If enabled, the bar will automatically adjust the length to reflect a round number (starting
+	 * with 1, 2 or 5). If disabled, the bar will always be drawn in full length representing a
+	 * fractional distance.
+	 */
+	public void setEnableAdjustLength(boolean adjustLength) {
+		this.adjustLength = adjustLength;
+		lastZoomLevel = -1; // Force redraw of scalebar
+	}
+
+	/**
+	 * Sets the maximum bar length. If adjustLength is disabled this will match exactly the length of the bar.
+	 * If adjustLength is enabled, the bar will be shortened to reflect a round number in length. 
+	 * @param pMaxLengthInCm maximum length of the bar in the screen in cm. Default is 2.54 (=1 inch)
+	 */
+	public void setMaxLength(final float pMaxLengthInCm) {
+		this.maxLength = pMaxLengthInCm;
+		lastZoomLevel = -1; // Force redraw of scalebar
 	}
 
 	// ===========================================================
@@ -261,6 +361,10 @@ public class ScaleBarOverlay extends SafeDrawOverlay implements GeoConstants {
 
 			mBounds.set(projection.getScreenRect());
 			mBounds.offset((int) xOffset, (int) yOffset);
+			if (centred && latitudeBar)
+				mBounds.offset(-scaleBarPicture.getWidth() / 2, 0);
+			if (centred && longitudeBar)
+				mBounds.offset(0, -scaleBarPicture.getHeight() / 2);
 
 			mBounds.set(mBounds.left, mBounds.top, mBounds.left + scaleBarPicture.getWidth(),
 					mBounds.top + scaleBarPicture.getHeight());
@@ -290,64 +394,148 @@ public class ScaleBarOverlay extends SafeDrawOverlay implements GeoConstants {
 			return;
 		}
 
-		// Two points, 1-inch apart in x/latitude, centered on screen
-		IGeoPoint p1 = projection.fromPixels((screenWidth / 2) - (xdpi / 2), screenHeight / 2);
-		IGeoPoint p2 = projection.fromPixels((screenWidth / 2) + (xdpi / 2), screenHeight / 2);
+		// calculate dots per centimeter
+		int xdpcm = (int) ((float) xdpi / 2.54);
+		int ydpcm = (int) ((float) ydpi / 2.54);
 
-		final int xMetersPerInch = ((GeoPoint) p1).distanceTo(p2);
+		// get length in pixel
+		int xLen = (int) (maxLength * xdpcm);
+		int yLen = (int) (maxLength * ydpcm);
 
-		p1 = projection.fromPixels(screenWidth / 2, (screenHeight / 2) - (ydpi / 2));
-		p2 = projection.fromPixels(screenWidth / 2, (screenHeight / 2) + (ydpi / 2));
+		// Two points, xLen apart, at scale bar screen location
+		IGeoPoint p1 = projection.fromPixels((screenWidth / 2) - (xLen / 2), yOffset);
+		IGeoPoint p2 = projection.fromPixels((screenWidth / 2) + (xLen / 2), yOffset);
 
-		final int yMetersPerInch = ((GeoPoint) p1).distanceTo(p2);
+		// get distance in meters between points
+		final int xMeters = ((GeoPoint) p1).distanceTo(p2);
+		// get adjusted distance, shortened to the next lower number starting with 1, 2 or 5
+		final double xMetersAdjusted = this.adjustLength ? adjustScaleBarLength(xMeters) : xMeters;
+		// get adjusted length in pixels
+		final int xBarLengthPixels = (int) (xLen * xMetersAdjusted / xMeters);
 
-		final Canvas canvas = scaleBarPicture.beginRecording((int) xdpi, (int) ydpi);
+		// Two points, yLen apart, at scale bar screen location
+		p1 = projection.fromPixels(screenWidth / 2, (screenHeight / 2) - (yLen / 2));
+		p2 = projection.fromPixels(screenWidth / 2, (screenHeight / 2) + (yLen / 2));
 
-		if (latitudeBar) {
-			final String xMsg = scaleBarLengthText(xMetersPerInch, imperial, nautical);
-			final Rect xTextRect = new Rect();
-			textPaint.getTextBounds(xMsg, 0, xMsg.length(), xTextRect);
+		// get distance in meters between points
+		final int yMeters = ((GeoPoint) p1).distanceTo(p2);
+		// get adjusted distance, shortened to the next lower number starting with 1, 2 or 5
+		final double yMetersAdjusted = this.adjustLength ? adjustScaleBarLength(yMeters) : yMeters;
+		// get adjusted length in pixels
+		final int yBarLengthPixels = (int) (yLen * yMetersAdjusted / yMeters);
 
-			final int textSpacing = (int) (xTextRect.height() / 5.0);
+		final Canvas canvas = scaleBarPicture.beginRecording(xBarLengthPixels, yBarLengthPixels);
 
-			canvas.drawRect(0, 0, xdpi, lineWidth, barPaint);
-			canvas.drawRect(xdpi, 0, xdpi + lineWidth,
-					xTextRect.height() + lineWidth + textSpacing, barPaint);
+		// create text
+		final String xMsg = scaleBarLengthText((int) xMetersAdjusted, imperial, nautical);
+		final Rect xTextRect = new Rect();
+		textPaint.getTextBounds(xMsg, 0, xMsg.length(), xTextRect);
+		final int xTextSpacing = (int) (xTextRect.height() / 5.0);
 
-			if (!longitudeBar) {
-				canvas.drawRect(0, 0, lineWidth, xTextRect.height() + lineWidth + textSpacing,
-						barPaint);
-			}
+		final String yMsg = scaleBarLengthText((int) yMetersAdjusted, imperial, nautical);
+		final Rect yTextRect = new Rect();
+		textPaint.getTextBounds(yMsg, 0, yMsg.length(), yTextRect);
+		final int yTextSpacing = (int) (yTextRect.height() / 5.0);
 
-			canvas.drawText(xMsg, xdpi / 2 - xTextRect.width() / 2, xTextRect.height() + lineWidth
-					+ textSpacing, textPaint);
+		// paint background
+		if (bgPaint != null) {
+			canvas.drawRect(0, 0, yTextRect.height() + 2 * lineWidth + yTextSpacing,
+					xTextRect.height() + 2 * lineWidth + xTextSpacing, bgPaint);
+			if (latitudeBar)
+				canvas.drawRect(yTextRect.height() + 2 * lineWidth + yTextSpacing, 0,
+						xBarLengthPixels + lineWidth, xTextRect.height() + 2 * lineWidth
+								+ xTextSpacing, bgPaint);
+			if (longitudeBar)
+				canvas.drawRect(0, xTextRect.height() + 2 * lineWidth + xTextSpacing,
+						yTextRect.height() + 2 * lineWidth + yTextSpacing, yBarLengthPixels
+								+ lineWidth, bgPaint);
 		}
 
-		if (longitudeBar) {
-			final String yMsg = scaleBarLengthText(yMetersPerInch, imperial, nautical);
-			final Rect yTextRect = new Rect();
-			textPaint.getTextBounds(yMsg, 0, yMsg.length(), yTextRect);
+		// draw latitude bar
+		if (latitudeBar) {
+			canvas.drawRect(0, 0, xBarLengthPixels, lineWidth, barPaint);
+			canvas.drawRect(xBarLengthPixels, 0, xBarLengthPixels + lineWidth, xTextRect.height()
+					+ lineWidth + xTextSpacing, barPaint);
 
-			final int textSpacing = (int) (yTextRect.height() / 5.0);
-
-			canvas.drawRect(0, 0, lineWidth, ydpi, barPaint);
-			canvas.drawRect(0, ydpi, yTextRect.height() + lineWidth + textSpacing,
-					ydpi + lineWidth, barPaint);
-
-			if (!latitudeBar) {
-				canvas.drawRect(0, 0, yTextRect.height() + lineWidth + textSpacing, lineWidth,
+			if (!longitudeBar) {
+				canvas.drawRect(0, 0, lineWidth, xTextRect.height() + lineWidth + xTextSpacing,
 						barPaint);
 			}
 
-			final float x = yTextRect.height() + lineWidth + textSpacing;
-			final float y = ydpi / 2 + yTextRect.width() / 2;
+			canvas.drawText(xMsg, xBarLengthPixels / 2 - xTextRect.width() / 2, xTextRect.height()
+					+ lineWidth + xTextSpacing, textPaint);
+		}
+
+		// draw longitude bar
+		if (longitudeBar) {
+			canvas.drawRect(0, 0, lineWidth, yBarLengthPixels, barPaint);
+			canvas.drawRect(0, yBarLengthPixels, yTextRect.height() + lineWidth + yTextSpacing,
+					yBarLengthPixels + lineWidth, barPaint);
+
+			if (!latitudeBar) {
+				canvas.drawRect(0, 0, yTextRect.height() + lineWidth + yTextSpacing, lineWidth,
+						barPaint);
+			}
+
+			final float x = yTextRect.height() + lineWidth + yTextSpacing;
+			final float y = yBarLengthPixels / 2 + yTextRect.width() / 2;
 
 			canvas.rotate(-90, x, y);
-			canvas.drawText(yMsg, x, y + textSpacing, textPaint);
-
+			canvas.drawText(yMsg, x, y, textPaint);
 		}
 
 		scaleBarPicture.endRecording();
+	}
+
+	/**
+	 * Returns a reduced length that starts with 1, 2 or 5 and trailing zeros. If set to nautical or imperial the 
+	 * input will be transformed before and after the reduction so that the result holds in that respective unit.
+	 * @param length length to round
+	 * @return reduced, rounded (in m, nm or mi depending on setting) result
+	 */
+	private double adjustScaleBarLength(double length) {
+		long pow = 0;
+		boolean feet = false;
+		if (this.imperial) {
+			if (length >= GeoConstants.METERS_PER_STATUTE_MILE / 5)
+				length = length / GeoConstants.METERS_PER_STATUTE_MILE;
+			else {
+				length = length * GeoConstants.FEET_PER_METER;
+				feet = true;
+			}
+		} else if (this.nautical) {
+			if (length >= GeoConstants.METERS_PER_NAUTICAL_MILE / 5)
+				length = length / GeoConstants.METERS_PER_NAUTICAL_MILE;
+			else {
+				length = length * GeoConstants.FEET_PER_METER;
+				feet = true;
+			}
+		}
+
+		while (length >= 10) {
+			pow++;
+			length /= 10;
+		}
+		while (length < 1 && length > 0) {
+			pow--;
+			length *= 10;
+		}
+
+		if (length < 2) {
+			length = 1;
+		} else if (length < 5) {
+			length = 2;
+		} else {
+			length = 5;
+		}
+		if (feet)
+			length = length / GeoConstants.FEET_PER_METER;
+		else if (this.imperial)
+			length = length * GeoConstants.METERS_PER_STATUTE_MILE;
+		else if (this.nautical)
+			length = length * GeoConstants.METERS_PER_NAUTICAL_MILE;
+		length *= Math.pow(10, pow);
+		return length;
 	}
 
 	protected String scaleBarLengthText(final int meters, final boolean imperial,
