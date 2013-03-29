aeebe2f8a024a1c07d78e76d92173f5d64d9a25d
kurtzmarc@gmail.com <kurtzmarc@gmail.com@349629ee-b03d-11dd-8830-69e4bb380a4a>
2013-03-29 01:51:27 +0000

    Checking in new modernized samples (take 2). Added MapFragment. Added RotationGestureOverlay.

--- OpenStreetMapViewer/src/org/osmdroid/MapActivity.java
+++ OpenStreetMapViewer/src/org/osmdroid/MapActivity.java
@@ -1,248 +1,65 @@
 // Created by plusminus on 00:23:14 - 03.10.2008
 package org.osmdroid;
 
-import org.osmdroid.constants.OpenStreetMapConstants;
-import org.osmdroid.samples.SampleLoader;
-import org.osmdroid.tileprovider.tilesource.ITileSource;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.tileprovider.tilesource.bing.BingMapTileSource;
-import org.osmdroid.tileprovider.util.CloudmadeUtil;
-import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.MyLocationOverlay;
-import org.osmdroid.views.overlay.TilesOverlay;
-
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.widget.RelativeLayout;
-import android.widget.RelativeLayout.LayoutParams;
-import android.widget.Toast;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 /**
  * Default map view activity.
- *
+ * 
  * @author Manuel Stahl
- *
+ * 
  */
-public class MapActivity extends Activity implements OpenStreetMapConstants {
-	// ===========================================================
-	// Constants
-	// ===========================================================
+public class MapActivity extends FragmentActivity
+{
 
-	private static final int MENU_SAMPLES = Menu.FIRST + 1;
-	private static final int MENU_ABOUT = MENU_SAMPLES + 1;
+    private static final int DIALOG_ABOUT_ID = 1;
 
-	private static final int MENU_LAST_ID = MENU_ABOUT + 1; // Always set to last unused id
+    // ===========================================================
+    // Constructors
+    // ===========================================================
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(final Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
 
-	private static final int DIALOG_ABOUT_ID = 1;
+        this.setContentView(R.layout.main);
 
-	// ===========================================================
-	// Fields
-	// ===========================================================
+        // FrameLayout mapContainer = (FrameLayout) findViewById(R.id.map_container);
+        // RelativeLayout parentContainer = (RelativeLayout) findViewById(R.id.parent_container);
+        FragmentManager fm = this.getSupportFragmentManager();
 
-	private SharedPreferences mPrefs;
-	private MapView mOsmv;
-	private MyLocationOverlay mLocationOverlay;
-	private ResourceProxy mResourceProxy;
+        MapFragment mapFragment = new MapFragment();
 
-	// ===========================================================
-	// Constructors
-	// ===========================================================
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(final Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+        fm.beginTransaction().add(R.id.map_container, mapFragment).commit();
+    }
 
-		mResourceProxy = new ResourceProxyImpl(getApplicationContext());
+    @Override
+    protected Dialog onCreateDialog(final int id)
+    {
+        Dialog dialog;
 
-		mPrefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        switch (id) {
+            case DIALOG_ABOUT_ID:
+                return new AlertDialog.Builder(MapActivity.this).setIcon(R.drawable.icon)
+                        .setTitle(R.string.app_name).setMessage(R.string.about_message)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(final DialogInterface dialog, final int whichButton)
+                            {
+                                //
+                            }
+                        }).create();
 
-		// only do static initialisation if needed
-		if (CloudmadeUtil.getCloudmadeKey().length() == 0) {
-			CloudmadeUtil.retrieveCloudmadeKey(getApplicationContext());
-		}
-		if (BingMapTileSource.getBingKey().length() == 0) {
-			BingMapTileSource.retrieveBingKey(getApplicationContext());
-		}
-		final BingMapTileSource bmts = new BingMapTileSource(null);
-		if (!TileSourceFactory.containsTileSource(bmts.name())) {
-			TileSourceFactory.addTileSource(bmts);
-		}
-
-		final RelativeLayout rl = new RelativeLayout(this);
-		this.mOsmv = new MapView(this, 256, mResourceProxy);
-		this.mLocationOverlay = new MyLocationOverlay(this.getBaseContext(), this.mOsmv,
-				mResourceProxy);
-		this.mOsmv.setBuiltInZoomControls(true);
-		this.mOsmv.setMultiTouchControls(true);
-		this.mOsmv.getOverlays().add(this.mLocationOverlay);
-		rl.addView(this.mOsmv, new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT,
-				LayoutParams.FILL_PARENT));
-
-		this.setContentView(rl);
-
-		mOsmv.getController().setZoom(mPrefs.getInt(PREFS_ZOOM_LEVEL, 1));
-		mOsmv.scrollTo(mPrefs.getInt(PREFS_SCROLL_X, 0), mPrefs.getInt(PREFS_SCROLL_Y, 0));
-
-
-		/*
-		 * This is an example of usage of runOnFirstFix.
-		 * It looks more complicated than necessary because we need to create an
-		 * extra thread and a handler.
-		 * If you wanted to do a non-GUI thread then you wouldn't need the handler.
-		 */
-		if (DEBUGMODE) {
-			final Handler handler = new Handler();
-			mLocationOverlay.runOnFirstFix(new Runnable() {
-				@Override
-				public void run() {
-					handler.post(new Runnable() {
-						@Override
-						public void run() {
-							Toast.makeText(getApplicationContext(),
-									R.string.first_fix_message,
-									Toast.LENGTH_LONG).show();
-						}
-					});
-				}
-			});
-		}
-	}
-
-	@Override
-	protected void onPause() {
-		final SharedPreferences.Editor edit = mPrefs.edit();
-		edit.putString(PREFS_TILE_SOURCE, mOsmv.getTileProvider().getTileSource().name());
-		edit.putInt(PREFS_SCROLL_X, mOsmv.getScrollX());
-		edit.putInt(PREFS_SCROLL_Y, mOsmv.getScrollY());
-		edit.putInt(PREFS_ZOOM_LEVEL, mOsmv.getZoomLevel());
-		edit.putBoolean(PREFS_SHOW_LOCATION, mLocationOverlay.isMyLocationEnabled());
-		edit.putBoolean(PREFS_SHOW_COMPASS, mLocationOverlay.isCompassEnabled());
-		edit.commit();
-
-		this.mLocationOverlay.disableMyLocation();
-		this.mLocationOverlay.disableCompass();
-
-		super.onPause();
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		final String tileSourceName = mPrefs.getString(PREFS_TILE_SOURCE,
-				TileSourceFactory.DEFAULT_TILE_SOURCE.name());
-		try {
-			final ITileSource tileSource = TileSourceFactory.getTileSource(tileSourceName);
-			mOsmv.setTileSource(tileSource);
-		} catch (final IllegalArgumentException ignore) {
-		}
-		if (mPrefs.getBoolean(PREFS_SHOW_LOCATION, false)) {
-			this.mLocationOverlay.enableMyLocation();
-		}
-		if (mPrefs.getBoolean(PREFS_SHOW_COMPASS, false)) {
-			this.mLocationOverlay.enableCompass();
-		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(final Menu pMenu) {
-		// Put samples first
-		pMenu.add(0, MENU_SAMPLES, Menu.NONE, R.string.samples).setIcon(
-				android.R.drawable.ic_menu_gallery);
-
-		// Put overlay items next
-		mOsmv.getOverlayManager().onCreateOptionsMenu(pMenu, MENU_LAST_ID, mOsmv);
-
-		// Put "About" menu item last
-		pMenu.add(0, MENU_ABOUT, Menu.NONE, R.string.about).setIcon(
-				android.R.drawable.ic_menu_info_details);
-
-		return true;
-	}
-
-	@Override
-	public boolean onPrepareOptionsMenu(final Menu pMenu) {
-		mOsmv.getOverlayManager().onPrepareOptionsMenu(pMenu, MENU_LAST_ID, mOsmv);
-		return super.onPrepareOptionsMenu(pMenu);
-	}
-
-	@Override
-	public boolean onMenuItemSelected(final int featureId, final MenuItem item) {
-
-		// We can also respond to events in the overlays here
-		final int overlayItemId = item.getItemId() - MENU_LAST_ID;
-		if (overlayItemId == MyLocationOverlay.MENU_MY_LOCATION) {
-			final int id = mLocationOverlay.isMyLocationEnabled() ? R.string.set_mode_hide_me
-					: R.string.set_mode_show_me;
-			Toast.makeText(this, id, Toast.LENGTH_LONG).show();
-		} else if (overlayItemId == TilesOverlay.MENU_OFFLINE) {
-			final int id = mOsmv.useDataConnection() ? R.string.set_mode_offline
-					: R.string.set_mode_online;
-			Toast.makeText(this, id, Toast.LENGTH_LONG).show();
-		}
-
-		// Now process the menu item selection
-		switch (item.getItemId()) {
-		case MENU_SAMPLES:
-			startActivity(new Intent(this, SampleLoader.class));
-			return true;
-
-		case MENU_ABOUT:
-			showDialog(DIALOG_ABOUT_ID);
-			return true;
-
-		default:
-			return mOsmv.getOverlayManager().onOptionsItemSelected(item, MENU_LAST_ID, mOsmv);
-		}
-	}
-
-	@Override
-	protected Dialog onCreateDialog(final int id) {
-		Dialog dialog;
-
-		switch (id) {
-		case DIALOG_ABOUT_ID:
-			return new AlertDialog.Builder(MapActivity.this).setIcon(R.drawable.icon)
-					.setTitle(R.string.app_name).setMessage(R.string.about_message)
-					.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-						@Override
-						public void onClick(final DialogInterface dialog, final int whichButton) {
-						}
-					}).create();
-
-		default:
-			dialog = null;
-			break;
-		}
-		return dialog;
-	}
-
-	// ===========================================================
-	// Getter & Setter
-	// ===========================================================
-
-	// ===========================================================
-	// Methods from SuperClass/Interfaces
-	// ===========================================================
-
-	@Override
-	public boolean onTrackballEvent(final MotionEvent event) {
-		return this.mOsmv.onTrackballEvent(event);
-	}
-
-	// ===========================================================
-	// Methods
-	// ===========================================================
-
-	// ===========================================================
-	// Inner and Anonymous Classes
-	// ===========================================================
+            default:
+                dialog = null;
+                break;
+        }
+        return dialog;
+    }
 }
---
+++ OpenStreetMapViewer/src/org/osmdroid/MapFragment.java
@@ -0,0 +1,198 @@
+// Created by plusminus on 00:23:14 - 03.10.2008
+package org.osmdroid;
+
+import org.osmdroid.constants.OpenStreetMapConstants;
+import org.osmdroid.samples.SampleLoader;
+import org.osmdroid.tileprovider.tilesource.ITileSource;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.tileprovider.util.CloudmadeUtil;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MinimapOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.*;
+
+/**
+ * Default map view activity.
+ * 
+ * @author Manuel Stahl
+ * 
+ */
+public class MapFragment extends Fragment implements OpenStreetMapConstants
+{
+    // ===========================================================
+    // Constants
+    // ===========================================================
+
+    private static final int DIALOG_ABOUT_ID = 1;
+
+    private static final int MENU_SAMPLES = Menu.FIRST + 1;
+    private static final int MENU_ABOUT = MENU_SAMPLES + 1;
+
+    private static final int MENU_LAST_ID = MENU_ABOUT + 1; // Always set to last unused id
+
+    // ===========================================================
+    // Fields
+    // ===========================================================
+
+    private SharedPreferences mPrefs;
+    private MapView mMapView;
+    private MyLocationNewOverlay mLocationOverlay;
+    private CompassOverlay mCompassOverlay;
+    private MinimapOverlay mMinimapOverlay;
+    private RotationGestureOverlay mRotationGestureOverlay;
+    private ResourceProxy mResourceProxy;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        mResourceProxy = new ResourceProxyImpl(inflater.getContext().getApplicationContext());
+        mMapView = new MapView(inflater.getContext(), 256, mResourceProxy);
+        mMapView.setUseSafeCanvas(true);
+        setHardwareAccelerationOff();
+        return mMapView;
+    }
+
+    @SuppressLint("NewApi")
+    private void setHardwareAccelerationOff()
+    {
+        // Turn off hardware acceleration here, or in manifest
+        if (android.os.Build.VERSION.SDK_INT >= 11)
+            mMapView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+
+        final Context context = this.getActivity();
+        // mResourceProxy = new ResourceProxyImpl(getActivity().getApplicationContext());
+
+        mPrefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+
+        // only do static initialisation if needed
+        if (CloudmadeUtil.getCloudmadeKey().length() == 0) {
+            CloudmadeUtil.retrieveCloudmadeKey(context.getApplicationContext());
+        }
+
+        this.mCompassOverlay = new CompassOverlay(context, new InternalCompassOrientationProvider(context),
+                mMapView);
+        this.mLocationOverlay = new MyLocationNewOverlay(context, new GpsMyLocationProvider(context),
+                mMapView);
+        mMinimapOverlay = new MinimapOverlay(getActivity(), mMapView.getTileRequestCompleteHandler());
+
+        mRotationGestureOverlay = new RotationGestureOverlay(context, mMapView);
+
+        mMapView.setBuiltInZoomControls(true);
+        mMapView.setMultiTouchControls(true);
+        mMapView.getOverlays().add(this.mLocationOverlay);
+        mMapView.getOverlays().add(this.mCompassOverlay);
+        mMapView.getOverlays().add(this.mMinimapOverlay);
+        mMapView.getOverlays().add(this.mRotationGestureOverlay);
+
+        mMapView.getController().setZoom(mPrefs.getInt(PREFS_ZOOM_LEVEL, 1));
+        mMapView.scrollTo(mPrefs.getInt(PREFS_SCROLL_X, 0), mPrefs.getInt(PREFS_SCROLL_Y, 0));
+
+        mLocationOverlay.enableMyLocation(new GpsMyLocationProvider(context));
+        mCompassOverlay.enableCompass(new InternalCompassOrientationProvider(context));
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onPause()
+    {
+        final SharedPreferences.Editor edit = mPrefs.edit();
+        edit.putString(PREFS_TILE_SOURCE, mMapView.getTileProvider().getTileSource().name());
+        edit.putInt(PREFS_SCROLL_X, mMapView.getScrollX());
+        edit.putInt(PREFS_SCROLL_Y, mMapView.getScrollY());
+        edit.putInt(PREFS_ZOOM_LEVEL, mMapView.getZoomLevel());
+        edit.putBoolean(PREFS_SHOW_LOCATION, mLocationOverlay.isMyLocationEnabled());
+        edit.putBoolean(PREFS_SHOW_COMPASS, mCompassOverlay.isCompassEnabled());
+        edit.commit();
+
+        this.mLocationOverlay.disableMyLocation();
+        this.mCompassOverlay.disableCompass();
+
+        super.onPause();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        final String tileSourceName = mPrefs.getString(PREFS_TILE_SOURCE,
+                TileSourceFactory.DEFAULT_TILE_SOURCE.name());
+        try {
+            final ITileSource tileSource = TileSourceFactory.getTileSource(tileSourceName);
+            mMapView.setTileSource(tileSource);
+        } catch (final IllegalArgumentException ignore) {
+        }
+        if (mPrefs.getBoolean(PREFS_SHOW_LOCATION, false)) {
+            this.mLocationOverlay.enableMyLocation(this.mLocationOverlay.getMyLocationProvider());
+        }
+        if (mPrefs.getBoolean(PREFS_SHOW_COMPASS, false)) {
+            this.mCompassOverlay.enableCompass(this.mCompassOverlay.getOrientationProvider());
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        // Put overlay items first
+        mMapView.getOverlayManager().onCreateOptionsMenu(menu, MENU_LAST_ID, mMapView);
+
+        // Put samples next
+        menu.add(0, MENU_SAMPLES, Menu.NONE, R.string.samples).setIcon(android.R.drawable.ic_menu_gallery);
+
+        // Put "About" menu item last
+        menu.add(0, MENU_ABOUT, Menu.CATEGORY_SECONDARY, R.string.about).setIcon(
+                android.R.drawable.ic_menu_info_details);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(final Menu pMenu)
+    {
+        mMapView.getOverlayManager().onPrepareOptionsMenu(pMenu, MENU_LAST_ID, mMapView);
+        super.onPrepareOptionsMenu(pMenu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        mMapView.getOverlayManager().onOptionsItemSelected(item, MENU_LAST_ID, mMapView);
+
+        switch (item.getItemId()) {
+            case MENU_SAMPLES:
+                startActivity(new Intent(getActivity(), SampleLoader.class));
+                return true;
+            case MENU_ABOUT:
+                getActivity().showDialog(DIALOG_ABOUT_ID);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // @Override
+    // public boolean onTrackballEvent(final MotionEvent event) {
+    // return this.mMapView.onTrackballEvent(event);
+    // }
+}
