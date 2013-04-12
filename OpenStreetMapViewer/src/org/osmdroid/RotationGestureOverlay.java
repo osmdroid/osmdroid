6847e216f8336d426acbe63a7b52caf0e287225a
kurtzmarc@gmail.com <kurtzmarc@gmail.com@349629ee-b03d-11dd-8830-69e4bb380a4a>
2013-04-12 00:28:39 +0000

    Added optional rotate menu items. Mostly for debugging in the emulator when we can't use multi-touch.

--- OpenStreetMapViewer/src/org/osmdroid/RotationGestureOverlay.java
+++ OpenStreetMapViewer/src/org/osmdroid/RotationGestureOverlay.java
@@ -13,7 +13,11 @@ import android.view.MotionEvent;
 public class RotationGestureOverlay extends SafeDrawOverlay implements
         RotationGestureDetector.RotationListener, IOverlayMenuProvider
 {
-    private final static int MENU_ENABLED = getSafeMenuId();
+	private final static boolean SHOW_ROTATE_MENU_ITEMS = false;
+
+	private final static int MENU_ENABLED = getSafeMenuId();
+	private final static int MENU_ROTATE_CCW = getSafeMenuId();
+	private final static int MENU_ROTATE_CW = getSafeMenuId();
 
     private final RotationGestureDetector mRotationDetector;
     private MapView mMapView;
@@ -56,26 +60,36 @@ public class RotationGestureOverlay extends SafeDrawOverlay implements
     @Override
     public boolean onCreateOptionsMenu(Menu pMenu, int pMenuIdOffset, MapView pMapView)
     {
-        pMenu.add(0, MENU_ENABLED + pMenuIdOffset, Menu.NONE, "Enable rotation").setIcon(
-                android.R.drawable.ic_menu_info_details);
+		pMenu.add(0, MENU_ENABLED + pMenuIdOffset, Menu.NONE, "Enable rotation").setIcon(
+				android.R.drawable.ic_menu_info_details);
+		if (SHOW_ROTATE_MENU_ITEMS) {
+			pMenu.add(0, MENU_ROTATE_CCW + pMenuIdOffset, Menu.NONE,
+					"Rotate maps counter clockwise").setIcon(android.R.drawable.ic_menu_rotate);
+			pMenu.add(0, MENU_ROTATE_CW + pMenuIdOffset, Menu.NONE, "Rotate maps clockwise")
+					.setIcon(android.R.drawable.ic_menu_rotate);
+		}
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem pItem, int pMenuIdOffset, MapView pMapView)
-    {
-        if (pItem.getItemId() == MENU_ENABLED + pMenuIdOffset) {
-            if (this.isEnabled()) {
-                mMapView.setMapOrientation(0);
-                this.setEnabled(false);
-            } else {
-                this.setEnabled(true);
-            }
-            return true;
-        }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem pItem, int pMenuIdOffset, MapView pMapView)
+	{
+		if (pItem.getItemId() == MENU_ENABLED + pMenuIdOffset) {
+			if (this.isEnabled()) {
+				mMapView.setMapOrientation(0);
+				this.setEnabled(false);
+			} else {
+				this.setEnabled(true);
+				return true;
+			}
+		} else if (pItem.getItemId() == MENU_ROTATE_CCW + pMenuIdOffset) {
+			mMapView.setMapOrientation(mMapView.getMapOrientation() - 10);
+		} else if (pItem.getItemId() == MENU_ROTATE_CW + pMenuIdOffset) {
+			mMapView.setMapOrientation(mMapView.getMapOrientation() + 10);
+		}
 
-        return false;
-    }
+		return false;
+	}
 
     @Override
     public boolean onPrepareOptionsMenu(final Menu pMenu, final int pMenuIdOffset, final MapView pMapView)
