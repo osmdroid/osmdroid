06f6e691977cbda1dbb91bd91a91fd7208e6cc98
kurtzmarc@gmail.com <kurtzmarc@gmail.com@349629ee-b03d-11dd-8830-69e4bb380a4a>
2013-04-12 04:50:02 +0000

    Updates to SampleItemizedOverlay.

--- OpenStreetMapViewer/src/org/osmdroid/SampleItemizedOverlay.java
+++ OpenStreetMapViewer/src/org/osmdroid/SampleItemizedOverlay.java
@@ -1,38 +1,100 @@
 package org.osmdroid;
 
+import org.osmdroid.ResourceProxy.bitmap;
 import org.osmdroid.api.IMapView;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ItemizedOverlay;
 import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.OverlayItem.HotspotPlace;
+import org.osmdroid.views.safecanvas.ISafeCanvas;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TextView;
 
-public class SampleItemizedOverlay extends ItemizedOverlay<OverlayItem> {
+public class SampleItemizedOverlay extends ItemizedOverlay<SampleOverlayItem> implements
+		ItemizedOverlay.OnFocusChangeListener {
+
+	private boolean mFocusChanged = false;
+	private View mPopupView = null;
 
 	public SampleItemizedOverlay(Drawable pDefaultMarker, Context pContext) {
-		this(pDefaultMarker, new DefaultResourceProxyImpl(pContext));
+		super(pDefaultMarker, new DefaultResourceProxyImpl(pContext));
+		populate();
+		setOnFocusChangeListener(this);
 	}
 
-	public SampleItemizedOverlay(Drawable pDefaultMarker, ResourceProxy pResourceProxy) {
-		super(pDefaultMarker, pResourceProxy);
-		// TODO Auto-generated constructor stub
+	@Override
+	protected SampleOverlayItem createItem(int i) {
+		SampleOverlayItem item;
+		if (i == 0)
+			item = new SampleOverlayItem("CentralPark", "Central Park",
+					"Central Park in New York City", new GeoPoint(40.7820, -73.9660), null,
+					HotspotPlace.BOTTOM_CENTER);
+		else
+			item = new SampleOverlayItem("NorthCentralPark", "North Central Park",
+					"North of Central Park in New York City", new GeoPoint(41.7820, -73.9660),
+					mResourceProxy.getDrawable(bitmap.person), HotspotPlace.CENTER);
+		return item;
+	}
+
+	@Override
+	public void onFocusChanged(ItemizedOverlay<?> overlay, OverlayItem newFocus) {
+		mFocusChanged = true;
+	}
+
+	@Override
+	protected boolean onTap(int index) {
+		setFocus(getItem(index));
+		return true;
+	}
+
+	@Override
+	protected void drawSafe(ISafeCanvas canvas, MapView mapView, boolean shadow) {
+		if (mFocusChanged) {
+			mFocusChanged = false;
+
+			// Remove any current focus
+			if (mPopupView != null)
+				mapView.removeView(mPopupView);
+
+			SampleOverlayItem item = this.getFocus();
+			if (item != null) {
+				mPopupView = getPopupView(mapView.getContext(), item);
+				MapView.LayoutParams lp = new MapView.LayoutParams(LayoutParams.WRAP_CONTENT,
+						LayoutParams.WRAP_CONTENT, item.getPoint(),
+						MapView.LayoutParams.TOP_CENTER, 0, 0);
+				mapView.addView(mPopupView, lp);
+			}
+		}
+		super.drawSafe(canvas, mapView, shadow);
+	}
+
+	protected View getPopupView(Context context, SampleOverlayItem item) {
+		TextView tv = new TextView(context);
+		tv.setText(item.getTitle());
+		tv.setBackgroundColor(Color.BLACK);
+		return tv;
+	}
+
+	@Override
+	protected void onDrawItem(Canvas canvas, SampleOverlayItem item, Point curScreenCoords) {
+		super.onDrawItem(canvas, item, curScreenCoords);
+	}
+
+	@Override
+	public int size() {
+		return 2;
 	}
 
 	@Override
 	public boolean onSnapToItem(int arg0, int arg1, Point arg2, IMapView arg3) {
 		return false;
-	}
-
-	@Override
-	protected OverlayItem createItem(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 }
--- OpenStreetMapViewer/src/org/osmdroid/SampleOverlayItem.java
+++ OpenStreetMapViewer/src/org/osmdroid/SampleOverlayItem.java
@@ -11,7 +11,7 @@ public class SampleOverlayItem extends OverlayItem {
 	public SampleOverlayItem(String aUid, String aTitle, String aDescription, GeoPoint aGeoPoint,
 			Drawable aMarker, HotspotPlace aHotspotPlace) {
 		super(aUid, aTitle, aDescription, aGeoPoint);
-		this.setMarker(mMarker);
+		this.setMarker(aMarker);
 		this.setMarkerHotspot(aHotspotPlace);
 	}
 
