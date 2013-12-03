9672bb8e123d5cca0a44ed11c4fc5804c615ebca
neilboyd@gmail.com <neilboyd@gmail.com@349629ee-b03d-11dd-8830-69e4bb380a4a>
2013-12-03 21:44:52 +0000

    An extra check for Google Maps v1 support

--- osmdroid-third-party/src/main/java/org/osmdroid/google/wrapper/v2/MapFactory.java
+++ osmdroid-third-party/src/main/java/org/osmdroid/google/wrapper/v2/MapFactory.java
@@ -1,5 +1,7 @@
 package org.osmdroid.google.wrapper.v2;
 
+import java.lang.reflect.Method;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -10,6 +12,7 @@ import org.osmdroid.api.IMap;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ConfigurationInfo;
+import android.content.pm.PackageManager;
 
 public class MapFactory {
 
@@ -45,6 +48,10 @@ public class MapFactory {
 		return new GoogleV1MapWrapper(aMapView);
 	}
 
+	/**
+	 * Check whether Google Maps v1 is supported on this device.
+	 * As an extra check you might also want to call {@link #canGetMapsFingerprint}
+	 */
 	public static boolean isGoogleMapsV1Supported() {
 		try {
 			Class.forName("com.google.android.maps.MapActivity");
@@ -54,6 +61,9 @@ public class MapFactory {
 		return false;
 	}
 
+	/**
+	 * Check whether Google Maps v2 is supported on this device.
+	 */
 	public static boolean isGoogleMapsV2Supported(final Context aContext) {
 		try {
 			// first check if Google Play Services is available
@@ -69,4 +79,22 @@ public class MapFactory {
 		}
 		return false;
 	}
+
+	/**
+	 * Sometimes {@link #isGoogleMapsV1Supported} can give a false positive which causes a subsequent
+	 * error when starting the map activity.
+	 * This method can be called as an extra check.
+	 */
+	public static boolean canGetMapsFingerprint(final PackageManager pm, final String packageName) {
+		try {
+			final Class<?> cls = Class.forName("com.google.android.maps.KeyHelper");
+			final Method gsf = cls.getDeclaredMethod("getSignatureFingerprint", new Class[]{PackageManager.class, String.class});
+			gsf.setAccessible(true);
+			final Object fingerprint = gsf.invoke(null, new Object[]{pm, packageName});
+			return true;
+		} catch (final Throwable e) {
+		}
+		return false;
+	}
+
 }
