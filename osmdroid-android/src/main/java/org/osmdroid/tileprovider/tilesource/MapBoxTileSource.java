362a43dcfba62454747c552caa0499c97aa09a99
neilboyd@gmail.com <neilboyd@gmail.com@349629ee-b03d-11dd-8830-69e4bb380a4a>
2014-02-09 21:16:39 +0000

    Use "farming" mechanism. Set max zoom level to be 18. See issue 521.

--- osmdroid-android/src/main/java/org/osmdroid/tileprovider/tilesource/MapBoxTileSource.java
+++ osmdroid-android/src/main/java/org/osmdroid/tileprovider/tilesource/MapBoxTileSource.java
@@ -16,9 +16,13 @@ public class MapBoxTileSource extends OnlineTileSourceBase
     /** the meta data key in the manifest */
     private static final String MAPBOX_MAPID = "MAPBOX_MAPID";
 
-	private static final String[] mapBoxBaseUrl = new String[] { "http://api.tiles.mapbox.com/v3/" };
+	private static final String[] mapBoxBaseUrl = new String[]{
+			"http://a.tiles.mapbox.com/v3/",
+			"http://b.tiles.mapbox.com/v3/",
+			"http://c.tiles.mapbox.com/v3/",
+			"http://d.tiles.mapbox.com/v3/"};
 
-    private static String mapBoxMapId = "";
+	private static String mapBoxMapId = "";
 
 	/**
      * TileSource with configuration defaults set.
@@ -26,7 +30,7 @@ public class MapBoxTileSource extends OnlineTileSourceBase
      */
     public MapBoxTileSource()
     {
-		super("mbtiles", ResourceProxy.string.base, 1, 20, 256, ".png", mapBoxBaseUrl);
+		super("mbtiles", ResourceProxy.string.base, 1, 18, 256, ".png", mapBoxBaseUrl);
     }
 
     /**
@@ -41,8 +45,7 @@ public class MapBoxTileSource extends OnlineTileSourceBase
 	 */
     public MapBoxTileSource(String name, ResourceProxy.string resourceId, int zoomMinLevel, int zoomMaxLevel, int tileSizePixels, String imageFilenameEnding)
     {
-		super(name, resourceId, zoomMinLevel, zoomMaxLevel, tileSizePixels, imageFilenameEnding,
-				mapBoxBaseUrl);
+		super(name, resourceId, zoomMinLevel, zoomMaxLevel, tileSizePixels, imageFilenameEnding, mapBoxBaseUrl);
     }
 
     /**
