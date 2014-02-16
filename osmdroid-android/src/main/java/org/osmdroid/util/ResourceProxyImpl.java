2b4f0b246bde04a92be2f99d7c9380a18776ca63
neilboyd@gmail.com <neilboyd@gmail.com@349629ee-b03d-11dd-8830-69e4bb380a4a>
2014-02-16 20:15:10 +0000

    Add getBitmap to ResourceProxyImpl. Fixes issue 489.

--- osmdroid-android/src/main/java/org/osmdroid/util/ResourceProxyImpl.java
+++ osmdroid-android/src/main/java/org/osmdroid/util/ResourceProxyImpl.java
@@ -6,6 +6,8 @@ import org.osmdroid.DefaultResourceProxyImpl;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 
 /**
@@ -34,6 +36,12 @@ public class ResourceProxyImpl extends DefaultResourceProxyImpl {
 	public String getString(final string pResId, final Object... formatArgs) {
 		final int id = getId("string", pResId.name());
 		return id != 0 ? mResources.getString(id, formatArgs) : super.getString(pResId, formatArgs);
+	}
+
+	@Override
+	public Bitmap getBitmap(final bitmap pResId) {
+		final int id = getId("drawable", pResId.name());
+		return id != 0 ? BitmapFactory.decodeResource(mResources, id) : super.getBitmap(pResId);
 	}
 
 	@Override
