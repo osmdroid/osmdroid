024aa807bd117ed7f917163174c2d3f735f1528d
mathieu.kergall@gmail.com <mathieu.kergall@gmail.com@a1b53bf6-db27-9bef-da93-b0e99b91ed31>
2014-03-08 11:03:57 +0000

    MarkerClusterer reorg. (with a GridMarkerClusterer), for easier customization.

--- osmdroid-android/src/org/osmdroid/bonuspack/clustering/MarkerClusterer.java
+++ osmdroid-android/src/org/osmdroid/bonuspack/clustering/MarkerClusterer.java
@@ -2,19 +2,12 @@ package org.osmdroid.bonuspack.clustering;
 
 import java.util.ArrayList;
 import org.osmdroid.bonuspack.overlays.Marker;
-import org.osmdroid.util.BoundingBoxE6;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.SafeDrawOverlay;
 import org.osmdroid.views.safecanvas.ISafeCanvas;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Point;
-import android.graphics.Rect;
-import android.graphics.drawable.BitmapDrawable;
-import android.support.v4.util.LongSparseArray;
 import android.view.MotionEvent;
 
 /** 
@@ -22,20 +15,13 @@ import android.view.MotionEvent;
  * Usage: put your markers inside with add(Marker), and add the MarkerClusterer to the map overlays. 
  * Depending on the zoom level, markers will be displayed separately, or grouped as a single Marker. <br/>
  * 
- * Clustering algorithm is grid-based : all markers inside the same cell belong to the same cluster. 
- * The grid size is specified in screen pixels. <br/>
+ * This abstract class provides the framework. Sub-classes have to implement the clustering algorithm, 
+ * and the rendering of a cluster. 
  * 
- * TODO: check what happens with map rotation. <br/>
- * TODO: clustering is not perfectly stable: if you zoom in then out, the mapview lat/lon span may change a little bit,
- * potentially changing the grid positioning. <br/>
- * 
- * Largely inspired from (open source) Google Maps Android utility library. 
- * 
- * @see https://github.com/googlemaps/android-maps-utils
  * @author M.Kergall
  *
  */
-public class MarkerClusterer extends SafeDrawOverlay {
+public abstract class MarkerClusterer extends SafeDrawOverlay {
 
 	/** impossible value for zoom level, to force clustering */
 	protected static final int FORCE_CLUSTERING = -1;
@@ -43,35 +29,27 @@ public class MarkerClusterer extends SafeDrawOverlay {
 	protected ArrayList<Marker> mItems = new ArrayList<Marker>();
 	protected Point mPoint = new Point();
 	protected ArrayList<StaticCluster> mClusters = new ArrayList<StaticCluster>();;
-	protected int mLastZoomLevel = FORCE_CLUSTERING;
+	protected int mLastZoomLevel;
 	protected Bitmap mClusterIcon;
-	protected int mGridSize = 50; //in pixels
-	protected Paint mTextPaint;
 	
-	/** cluster icon anchor */
-	public float mAnchorU = Marker.ANCHOR_CENTER, mAnchorV = Marker.ANCHOR_CENTER;
-	/** anchor point to draw the number of markers inside the cluster icon */
-	public float mTextAnchorU = Marker.ANCHOR_CENTER, mTextAnchorV = Marker.ANCHOR_CENTER;
-
+	// abstract methods: 
+	
+	/** clustering algorithm */
+	public abstract ArrayList<StaticCluster> clusterer(MapView mapView);
+	/** Build the marker for a cluster. */
+	public abstract Marker buildClusterMarker(StaticCluster cluster, MapView mapView);
+	/** build clusters markers to be used at next draw */
+	public abstract void renderer(ArrayList<StaticCluster> clusters, ISafeCanvas canvas, MapView mapView);
+	
 	public MarkerClusterer(Context ctx) {
 		super(ctx);
-		mTextPaint = new Paint();
-		mTextPaint.setColor(Color.WHITE);
-		mTextPaint.setTextSize(17.0f);
-		mTextPaint.setFakeBoldText(true);
-		mTextPaint.setTextAlign(Paint.Align.CENTER);
-		mTextPaint.setAntiAlias(true);
+		mLastZoomLevel = FORCE_CLUSTERING;
 	}
 
 	/** Set the cluster icon to be drawn when a cluster contains more than 1 marker. 
-	 * Default will be the default osmdroid marker icon, which is really inappropriate as a cluster icon. */
+	 * If not set, default will be the default osmdroid marker icon (which is really inappropriate as a cluster icon). */
 	public void setIcon(Bitmap icon){
 		mClusterIcon = icon;
-	}
-	
-	/** If you want to change the default text paint (color, size, font) */
-	public Paint getTextPaint(){
-		return mTextPaint;
 	}
 	
 	/** Add the Marker. 
@@ -94,87 +72,6 @@ public class MarkerClusterer extends SafeDrawOverlay {
 	/** @return the list of Markers. */
 	public ArrayList<Marker> getItems(){
 		return mItems;
-	}
-	
-	/** Change the size of the clustering grid, in pixels. Default is 50px. */
-	public void setGridSize(int gridSize){
-		mGridSize = gridSize;
-	}
-
-	/** Grid-based clustering algorithm */
-	public ArrayList<StaticCluster> clusterer(MapView mapView){
-		Rect mScreenRect = mapView.getIntrinsicScreenRect(null);
-		
-		BoundingBoxE6 bb = mapView.getBoundingBox();
-		double latSpan = bb.getLatitudeSpanE6()*1E-6;
-		double lonSpan = bb.getLongitudeSpanE6()*1E-6;
-		//Log.d("ZOOM", "latSpan="+latSpan+"  lonSpan="+lonSpan);
-		
-		//convert grid size from pixels to degrees:
-		double gridSizeX, gridSizeY;
-		gridSizeX = lonSpan * (double)mGridSize / (double)(mScreenRect.right - mScreenRect.left);
-		gridSizeY = latSpan * (double)mGridSize / (double)(mScreenRect.bottom - mScreenRect.top);
-		
-		int numCellsW = (int)(360.0f/gridSizeX);
-		//Log.d("ZOOM", "zoomlevel="+mapView.getZoomLevel()+"  cells="+numCellsW);
-		//Log.d("ZOOM", "gridSizeX="+gridSizeX+"  gridSizeY="+gridSizeY);
-		
-		ArrayList<StaticCluster> clusters = new ArrayList<StaticCluster>();
-		LongSparseArray<StaticCluster> sparseArray = new LongSparseArray<StaticCluster>();
-		for (Marker item:mItems){
-			//TODO - add 180° to prevent negative values
-			long gridX = (long)(item.getPosition().getLongitude() / gridSizeX);
-			long gridY = (long)(item.getPosition().getLatitude() / gridSizeY);
-			long coord = numCellsW * gridX + gridY;
-			StaticCluster cluster = sparseArray.get(coord);
-			//Log.d("ZOOM", "coord="+coord+" =>cluster:"+(cluster==null?"new":"add"));
-			if (cluster == null) {
-				//GeoPoint clusterCorner = new GeoPoint(gridSizeY*(double)gridY, gridSizeX*(double)gridX);
-                cluster = new StaticCluster(item.getPosition() /*clusterCorner*/);
-                sparseArray.put(coord, cluster);
-                clusters.add(cluster);
-			}
-            cluster.add(item);
-		}
-		return clusters;
-	}
-	
-	/** Build the marker for a cluster. 
-	 * Uses the cluster icon, and displays inside the number of markers it contains. <br/>
-	 * In the standard Google coordinate system for Marker icons: <br/>
-	 * - The cluster icon is anchored at mAnchorU, mAnchorV. <br/>
-	 * - The text showing the number of markers is anchored at mTextAnchorU, mTextAnchorV. 
-	 * This text is centered horizontally and vertically. */
-	public Marker buildClusterMarker(StaticCluster cluster, MapView mapView){
-		Marker m = new Marker(mapView);
-		m.setPosition(cluster.getPosition());
-		m.setInfoWindow(null);
-		m.setAnchor(mAnchorU, mAnchorV);
-		Bitmap finalIcon = Bitmap.createBitmap(mClusterIcon.getWidth(), mClusterIcon.getHeight(), mClusterIcon.getConfig());
-		Canvas iconCanvas = new Canvas(finalIcon);
-		iconCanvas.drawBitmap(mClusterIcon, 0, 0, null);
-		String text = ""+cluster.getSize();
-	    int textHeight = (int) (mTextPaint.descent() + mTextPaint.ascent());
-		iconCanvas.drawText(text, 
-				mTextAnchorU*mClusterIcon.getWidth(), 
-				mTextAnchorV*mClusterIcon.getHeight() - textHeight/2, 
-				mTextPaint);
-		m.setIcon(new BitmapDrawable(mapView.getContext().getResources(), finalIcon));
-		return m;
-	}
-	
-	/** build clusters markers to be used at next draw */
-	public void renderer(ArrayList<StaticCluster> clusters, ISafeCanvas canvas, MapView mapView){
-		for  (StaticCluster cluster:clusters){
-			if (cluster.getSize()==1){
-				//cluster has only 1 marker => use it as it is:
-				cluster.setMarker(cluster.getItem(0));
-			} else {
-				//only draw 1 Marker at Cluster center, displaying number of Markers contained
-				Marker m = buildClusterMarker(cluster, mapView);
-				cluster.setMarker(m);
-			}
-		}
 	}
 	
 	@Override protected void drawSafe(ISafeCanvas canvas, MapView mapView, boolean shadow) {
