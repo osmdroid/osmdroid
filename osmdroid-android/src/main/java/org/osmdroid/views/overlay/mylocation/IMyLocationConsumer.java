f14cd9e93348160612912d1074071151c9e1b98c
Marc Kurtz <kurtzmarc@gmail.com>
2014-03-24 16:43:32 -0400

    Some javadoc for IMyLocationConsumer.

--- osmdroid-android/src/main/java/org/osmdroid/views/overlay/mylocation/IMyLocationConsumer.java
+++ osmdroid-android/src/main/java/org/osmdroid/views/overlay/mylocation/IMyLocationConsumer.java
@@ -3,5 +3,8 @@ package org.osmdroid.views.overlay.mylocation;
 import android.location.Location;
 
 public interface IMyLocationConsumer {
+	/**
+	 * Call when a provider has a new location to consume. This can be called on any thread.
+	 */
 	void onLocationChanged(Location location, IMyLocationProvider source);
 }
