6cf2c4d4440a3c661edabd68d7fb9a5e6483df4d
Marc Kurtz <kurtzmarc@gmail.com>
2014-03-28 11:09:53 -0400

    Added the ability to get access to both the non-accelerated bitmap backed canvas as well as the accelerated canvas if you need it.

--- osmdroid-android/src/main/java/org/osmdroid/views/overlay/NonAcceleratedOverlay.java
+++ osmdroid-android/src/main/java/org/osmdroid/views/overlay/NonAcceleratedOverlay.java
@@ -4,8 +4,11 @@ import org.osmdroid.ResourceProxy;
 import org.osmdroid.views.MapView;
 
 import android.content.Context;
-import android.graphics.*;
+import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.PorterDuff.Mode;
 import android.os.Build;
 
@@ -44,6 +47,13 @@ public abstract class NonAcceleratedOverlay extends Overlay
         super(pResourceProxy);
     }
 
+	/**
+	 * Override if you really want access to the original (possibly) accelerated canvas.
+	 */
+	protected void onDraw(Canvas c, Canvas acceleratedCanvas, MapView osmv, boolean shadow) {
+		onDraw(c, osmv, shadow);
+	}
+
     /**
      * Allow forcing this overlay to skip drawing using backing Bitmap by returning false.
      */
@@ -56,7 +66,7 @@ public abstract class NonAcceleratedOverlay extends Overlay
     protected final void draw(Canvas c, MapView osmv, boolean shadow)
     {
         // First check to see if we want to use the backing bitmap
-        boolean atLeastHoneycomb = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
+		final boolean atLeastHoneycomb = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
         if (isUsingBackingBitmap() && atLeastHoneycomb && c.isHardwareAccelerated()) {
             // Drawing a shadow layer would require a second backing Bitmap due to the way HW
             // accelerated drawBitmap works. One could extend this Overlay to implement that if needed.
@@ -72,7 +82,7 @@ public abstract class NonAcceleratedOverlay extends Overlay
             mBackingCanvas.drawColor(Color.TRANSPARENT, Mode.CLEAR);
             c.getMatrix(mBackingMatrix);
             mBackingCanvas.setMatrix(mBackingMatrix);
-            onDraw(mBackingCanvas, osmv, shadow);
+			onDraw(mBackingCanvas, c, osmv, shadow);
             c.save();
             c.getMatrix(mCanvasIdentityMatrix);
             mCanvasIdentityMatrix.invert(mCanvasIdentityMatrix);
@@ -80,6 +90,6 @@ public abstract class NonAcceleratedOverlay extends Overlay
             c.drawBitmap(mBackingBitmap, 0, 0, null);
             c.restore();
         } else
-            onDraw(c, osmv, shadow);
+			onDraw(c, c, osmv, shadow);
     }
 }
