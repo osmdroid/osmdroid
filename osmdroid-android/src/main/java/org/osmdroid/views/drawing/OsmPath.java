7472ec3c7ccb6ef9cd3c9e072ee6d224d4cb0485
Marc Kurtz <kurtzmarc@gmail.com>
2014-04-16 18:43:16 -0400

    Added some javadoc to OsmPath.

--- osmdroid-android/src/main/java/org/osmdroid/views/drawing/OsmPath.java
+++ osmdroid-android/src/main/java/org/osmdroid/views/drawing/OsmPath.java
@@ -6,6 +6,17 @@ import org.osmdroid.views.Projection;
 import android.graphics.Path;
 import android.graphics.Point;
 
+/**
+ * Since the osmdroid canvas coordinate system is changing with every scroll, the x/y coordinates of
+ * lat/long points is also always changing. Converting from lat/long to pixel values is a
+ * potentially expensive operation and shouldn't be performed in the draw() cycle. Instead of
+ * recalculating your {@link Path} points every draw cycle, you can use an OsmPath and call
+ * {@link #onDrawCycle(Projection)} at the start of your draw call. This will simply shift the Path
+ * the proper amount so that it is in the correct pixel position.
+ * 
+ * @author Marc Kurtz
+ * 
+ */
 public class OsmPath extends Path {
 
 	private final static GeoPoint sReferenceGeoPoint = new GeoPoint(0, 0);
@@ -20,6 +31,9 @@ public class OsmPath extends Path {
 		super(src);
 	}
 
+	/**
+	 * Call this method at the beginning of every draw() call.
+	 */
 	public void onDrawCycle(Projection proj) {
 		if (mLastZoomLevel != proj.getZoomLevel()) {
 			proj.toPixels(sReferenceGeoPoint, mReferencePoint);
