d01dca7eba70a1dac70390390512d3822c4afcd4
Marc Kurtz <kurtzmarc@gmail.com>
2014-04-17 14:51:10 -0400

    Updates #24 - fix for invalidating compass.

--- osmdroid-android/src/main/java/org/osmdroid/views/overlay/compass/CompassOverlay.java
+++ osmdroid-android/src/main/java/org/osmdroid/views/overlay/compass/CompassOverlay.java
@@ -105,18 +105,19 @@ public class CompassOverlay extends Overlay implements IOverlayMenuProvider, IOr
 
     private void invalidateCompass()
     {
-        Rect screenRect = mMapView.getProjection().getScreenRect();
-        final int frameLeft = screenRect.left + (mMapView.getWidth() / 2)
-                + (int) FloatMath.ceil((mCompassCenterX - mCompassFrameCenterX) * mScale);
-        final int frameTop = screenRect.top + (mMapView.getHeight() / 2)
-                + (int) FloatMath.ceil((mCompassCenterY - mCompassFrameCenterY) * mScale);
-        final int frameRight = screenRect.left + (mMapView.getWidth() / 2)
-                + (int) FloatMath.ceil((mCompassCenterX + mCompassFrameCenterX) * mScale);
-        final int frameBottom = screenRect.top + (mMapView.getHeight() / 2)
-                + (int) FloatMath.ceil((mCompassCenterY + mCompassFrameCenterY) * mScale);
+		Rect screenRect = mMapView.getProjection().getScreenRect();
+		final int frameLeft = screenRect.left
+				+ (int) FloatMath.ceil((mCompassCenterX - mCompassFrameCenterX) * mScale);
+		final int frameTop = screenRect.top
+				+ (int) FloatMath.ceil((mCompassCenterY - mCompassFrameCenterY) * mScale);
+		final int frameRight = screenRect.left
+				+ (int) FloatMath.ceil((mCompassCenterX + mCompassFrameCenterX) * mScale);
+		final int frameBottom = screenRect.top
+				+ (int) FloatMath.ceil((mCompassCenterY + mCompassFrameCenterY) * mScale);
 
-        // Expand by 2 to cover stroke width
-        mMapView.postInvalidate(frameLeft - 2, frameTop - 2, frameRight + 2, frameBottom + 2);
+		// Expand by 2 to cover stroke width
+		mMapView.invalidateMapCoordinates(frameLeft - 2, frameTop - 2, frameRight + 2,
+				frameBottom + 2);
     }
 
     // ===========================================================
