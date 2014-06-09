cf7e5a24b6fd1d3bd958a168af93dad880051853
mathieu.kergall@gmail.com <mathieu.kergall@gmail.com@a1b53bf6-db27-9bef-da93-b0e99b91ed31>
2014-06-09 20:55:05 +0000

    First commit

---
+++ MapsForgeForOsmdroid/src/org/osmdroid/bonuspack/mapsforge/MapsForgeTileModuleProvider.java
@@ -0,0 +1,81 @@
+package org.osmdroid.bonuspack.mapsforge;
+
+import java.io.File;
+import org.osmdroid.tileprovider.IRegisterReceiver;
+import org.osmdroid.tileprovider.MapTileRequestState;
+import org.osmdroid.tileprovider.modules.MapTileFileStorageProviderBase;
+import org.osmdroid.tileprovider.modules.MapTileModuleProviderBase;
+import org.osmdroid.tileprovider.tilesource.ITileSource;
+import android.graphics.drawable.Drawable;
+
+/**
+ * http://www.salidasoftware.com/how-to-render-mapsforge-tiles-in-osmdroid/
+ * @author Salida Software
+ * Adapted from code found here : http://www.sieswerda.net/2012/08/15/upping-the-developer-friendliness/
+ */
+public class MapsForgeTileModuleProvider extends MapTileFileStorageProviderBase {
+
+	protected MapsForgeTileSource tileSource;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param receiverRegistrar
+	 * @param file
+	 * @param tileSource
+	 */
+	public MapsForgeTileModuleProvider(IRegisterReceiver receiverRegistrar, File file, MapsForgeTileSource tileSource) {
+
+		super(receiverRegistrar, NUMBER_OF_TILE_FILESYSTEM_THREADS, TILE_FILESYSTEM_MAXIMUM_QUEUE_SIZE);
+
+		this.tileSource = tileSource;
+
+	}
+
+	@Override
+	protected String getName() {
+		return "MapsforgeTiles Provider";
+	}
+
+	@Override
+	protected String getThreadGroupName() {
+		return "mapsforgetilesprovider";
+	}
+
+	@Override
+	protected Runnable getTileLoader() {
+		return new TileLoader();
+	}
+
+	@Override
+	public boolean getUsesDataConnection() {
+		return false;
+	}
+
+	@Override
+	public int getMinimumZoomLevel() {
+		return tileSource.getMinimumZoomLevel();
+	}
+
+	@Override
+	public int getMaximumZoomLevel() {
+		return tileSource.getMaximumZoomLevel();
+	}
+
+	@Override
+	public void setTileSource(ITileSource tileSource) {
+		//prevent re-assignment of tile source
+		if (tileSource instanceof MapsForgeTileSource) {
+			this.tileSource = (MapsForgeTileSource) tileSource;
+		}
+	}
+
+	private class TileLoader extends MapTileModuleProviderBase.TileLoader {
+
+		@Override
+		public Drawable loadTile(final MapTileRequestState pState) {
+			return tileSource.renderTile(pState.getMapTile());
+		}
+	}
+
+}
---
+++ MapsForgeForOsmdroid/src/org/osmdroid/bonuspack/mapsforge/MapsForgeTileSource.java
@@ -0,0 +1,133 @@
+package org.osmdroid.bonuspack.mapsforge;
+
+import java.io.File;
+import org.mapsforge.map.reader.MapDatabase;
+import org.mapsforge.android.maps.DebugSettings;
+import org.mapsforge.android.maps.mapgenerator.JobParameters;
+import org.mapsforge.android.maps.mapgenerator.MapGeneratorJob;
+import org.mapsforge.android.maps.mapgenerator.databaserenderer.DatabaseRenderer;
+import org.mapsforge.map.reader.header.FileOpenResult;
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.ResourceProxy.string;
+import org.osmdroid.tileprovider.MapTile;
+import org.osmdroid.tileprovider.tilesource.BitmapTileSourceBase;
+import org.mapsforge.map.rendertheme.InternalRenderTheme;
+import org.mapsforge.map.rendertheme.XmlRenderTheme;
+import org.mapsforge.core.model.Tile;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
+/**
+ * http://www.salidasoftware.com/how-to-render-mapsforge-tiles-in-osmdroid/
+ * @author Salida Software
+ * Adapted from code found here : http://www.sieswerda.net/2012/08/15/upping-the-developer-friendliness/
+ */
+public class MapsForgeTileSource extends BitmapTileSourceBase {
+
+	protected File mapFile;
+
+	// Reasonable defaults ..
+	public static final int MIN_ZOOM = 8;
+	public static final int MAX_ZOOM = 20;
+	public static final int TILE_SIZE_PIXELS = 256;
+
+	private DatabaseRenderer renderer;
+	private MapDatabase mapDatabase;
+	private XmlRenderTheme jobTheme;
+	private JobParameters jobParameters;
+	private DebugSettings debugSettings;
+
+	// Required for the superclass
+	public static final string resourceId = ResourceProxy.string.offline_mode;
+
+	/**
+	 * The reason this constructor is protected is because all parameters,
+	 * except file should be determined from the archive file. Therefore a
+	 * factory method is necessary.
+	 * 
+	 * @param minZoom
+	 * @param maxZoom
+	 * @param tileSizePixels
+	 * @param file
+	 */
+	protected MapsForgeTileSource(int minZoom, int maxZoom, int tileSizePixels, File file) {
+		super("MapsForgeTiles", resourceId, minZoom, maxZoom, tileSizePixels, ".png");
+
+		mapDatabase = new MapDatabase();
+
+		//Make sure the database can open the file
+		FileOpenResult fileOpenResult = this.mapDatabase.openFile(file);
+		if (fileOpenResult.isSuccess()) {
+			mapFile = file;
+		}
+		else{
+			mapFile = null;
+		}
+
+		renderer = new DatabaseRenderer(mapDatabase);
+		minZoom = renderer.getStartZoomLevel();
+		maxZoom = renderer.getZoomLevelMax();
+		tileSizePixels = mapDatabase.getMapFileInfo().tilePixelSize;
+		Log.d("MAPSFORGE", "min="+minZoom+" max="+maxZoom+" tilesize="+tileSizePixels);
+
+		//  For this to work I had to edit org.mapsforge.map.rendertheme.InternalRenderTheme.getRenderThemeAsStream()  to:
+		//  return this.getClass().getResourceAsStream(this.absolutePath + this.file);
+		jobTheme = InternalRenderTheme.OSMARENDER;    		
+		jobParameters = new JobParameters(jobTheme, 1);
+		debugSettings = new DebugSettings(false, false, false);
+
+	}
+
+	/**
+	 * Creates a new MapsForgeTileSource from file.
+	 * 
+	 * Parameters minZoom and maxZoom are obtained from the
+	 * database. If they cannot be obtained from the DB, the default values as
+	 * defined by this class are used.
+	 * 
+	 * @param file
+	 * @return the tile source
+	 */
+	public static MapsForgeTileSource createFromFile(File file) {
+		//TODO - set these based on .map file info
+		int minZoomLevel = MIN_ZOOM;
+		int maxZoomLevel = MAX_ZOOM;
+		int tileSizePixels = TILE_SIZE_PIXELS;
+
+		return new MapsForgeTileSource(minZoomLevel, maxZoomLevel, tileSizePixels, file);
+	}
+
+	//The synchronized here is VERY important.  If missing, the mapDatabase read gets corrupted by multiple threads reading the file at once.
+	public synchronized Drawable renderTile(MapTile pTile) {
+
+		Tile tile = new Tile((long)pTile.getX(), (long)pTile.getY(), (byte)pTile.getZoomLevel());
+
+		//Create a bitmap to draw on
+		Bitmap bitmap = Bitmap.createBitmap(TILE_SIZE_PIXELS, TILE_SIZE_PIXELS, Bitmap.Config.RGB_565);
+
+		//You could try something like this to load a custom theme
+		//try{
+		//	jobTheme = new ExternalRenderTheme(themeFile);
+		//}
+		//catch(Exception e){
+		//	jobTheme = InternalRenderTheme.OSMARENDER;
+		//}
+
+		try{
+			//Draw the tile
+			MapGeneratorJob mapGeneratorJob = new MapGeneratorJob(tile, mapFile, jobParameters, debugSettings);
+			renderer.executeJob(mapGeneratorJob, bitmap);
+		}
+		catch(Exception ex){
+			//Make the bad tile easy to spot
+			bitmap.eraseColor(Color.YELLOW);
+		}
+
+		Drawable d = new BitmapDrawable(bitmap);
+		return d;
+	}
+
+}
