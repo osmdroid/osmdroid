b2f981edd08683a85e230f3a6510a02617b4b078
Neil Boyd <NeilBoyd@gmail.com>
2015-11-18 22:30:40 +0100

    If no permission to check network state, assume we have a network Closes #165

--- osmdroid-android/src/main/java/org/osmdroid/tileprovider/modules/NetworkAvailabliltyCheck.java
+++ osmdroid-android/src/main/java/org/osmdroid/tileprovider/modules/NetworkAvailabliltyCheck.java
@@ -1,32 +1,40 @@
 package org.osmdroid.tileprovider.modules;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
 
 /**
- * A straightforward network check implementation. NOTE: Requires
- * android.permission.ACCESS_NETWORK_STATE and android.permission.ACCESS_WIFI_STATE (?) and
- * android.permission.INTERNET (?)
- * 
+ * A straightforward network check implementation.
+ *
  * @author Marc Kurtz
- * 
  */
 
 public class NetworkAvailabliltyCheck implements INetworkAvailablityCheck {
 
 	private final ConnectivityManager mConnectionManager;
 	private final boolean mIsX86;
+	private final boolean mHasNetworkStatePermission;
 
 	public NetworkAvailabliltyCheck(final Context aContext) {
 		mConnectionManager = (ConnectivityManager) aContext
 				.getSystemService(Context.CONNECTIVITY_SERVICE);
 		mIsX86 = "Android-x86".equalsIgnoreCase(Build.BRAND);
+
+		mHasNetworkStatePermission = aContext.getPackageManager()
+				.checkPermission(Manifest.permission.ACCESS_NETWORK_STATE, aContext.getPackageName())
+				== PackageManager.PERMISSION_GRANTED;
 	}
 
 	@Override
 	public boolean getNetworkAvailable() {
+		if (!mHasNetworkStatePermission) {
+			// if we're unable to check network state, assume we have a network
+			return true;
+		}
 		final NetworkInfo networkInfo = mConnectionManager.getActiveNetworkInfo();
 		if (networkInfo == null) {
 			return false;
@@ -39,12 +47,21 @@ public class NetworkAvailabliltyCheck implements INetworkAvailablityCheck {
 
 	@Override
 	public boolean getWiFiNetworkAvailable() {
-		final NetworkInfo wifi = mConnectionManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		if (!mHasNetworkStatePermission) {
+			// if we're unable to check network state, assume we have a network
+			return true;
+		}
+		final NetworkInfo wifi = mConnectionManager
+				.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 		return wifi != null && wifi.isAvailable();
 	}
 
 	@Override
 	public boolean getCellularDataNetworkAvailable() {
+		if (!mHasNetworkStatePermission) {
+			// if we're unable to check network state, assume we have a network
+			return true;
+		}
 		final NetworkInfo mobile = mConnectionManager
 				.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
 		return mobile != null && mobile.isAvailable();
@@ -52,8 +69,9 @@ public class NetworkAvailabliltyCheck implements INetworkAvailablityCheck {
 
 	@Override
 	public boolean getRouteToPathExists(final int hostAddress) {
-		return (mConnectionManager.requestRouteToHost(ConnectivityManager.TYPE_WIFI, hostAddress) || mConnectionManager
-				.requestRouteToHost(ConnectivityManager.TYPE_MOBILE, hostAddress));
+		// TODO check for CHANGE_NETWORK_STATE permission
+		return mConnectionManager.requestRouteToHost(ConnectivityManager.TYPE_WIFI, hostAddress)
+			|| mConnectionManager.requestRouteToHost(ConnectivityManager.TYPE_MOBILE, hostAddress);
 	}
 
 }
