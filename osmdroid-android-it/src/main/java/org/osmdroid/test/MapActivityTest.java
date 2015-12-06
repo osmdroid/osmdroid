e6f2f6444b9e4d059075ee83b1e1b220f49c995b
Alex <alexoree@apache.org>
2015-12-05 19:31:22 -0500

    #203 integration tests are no longer running, fixed.

--- osmdroid-android-it/src/main/java/org/osmdroid/test/MapActivityTest.java
+++ osmdroid-android-it/src/main/java/org/osmdroid/test/MapActivityTest.java
@@ -7,7 +7,7 @@ import android.test.ActivityInstrumentationTestCase2;
 public class MapActivityTest extends ActivityInstrumentationTestCase2<MapActivity> {
 
     public MapActivityTest() {
-        super("org.osmdroid.example", MapActivity.class);
+        super("org.osmdroid", MapActivity.class);
     }
 
     public void testActivity() {
