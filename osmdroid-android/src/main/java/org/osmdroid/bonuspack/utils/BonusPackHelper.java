f1a55108ba8c01d56ac1f1932e58cee27548c176
Alex <alexoree@apache.org>
2016-01-25 18:28:40 -0500

    #252 adding back in a minified bonus pack helper class

---
+++ osmdroid-android/src/main/java/org/osmdroid/bonuspack/utils/BonusPackHelper.java
@@ -0,0 +1,22 @@
+package org.osmdroid.bonuspack.utils;
+
+/** Useful functions and common constants.
+ * @author M.Kergall
+ */
+public class BonusPackHelper {
+
+    /**
+     * Log tag.
+     */
+    public static final String LOG_TAG = "BONUSPACK";
+
+    /**
+     * resource id value meaning "undefined resource id"
+     */
+    public static final int UNDEFINED_RES_ID = 0;
+
+    /**
+     * User agent sent to services by default
+     */
+    public static final String DEFAULT_USER_AGENT = "OsmBonusPack/1";
+}
