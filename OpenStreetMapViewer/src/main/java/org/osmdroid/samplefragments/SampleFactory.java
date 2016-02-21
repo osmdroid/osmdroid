38e1f3cf09ea32d8d96767558371bde14dc535a1
Alex <alexoree@apache.org>
2016-02-21 09:25:36 -0500

    #272 fix for assets tile loading causing trashing

--- OpenStreetMapViewer/src/main/java/org/osmdroid/samplefragments/SampleFactory.java
+++ OpenStreetMapViewer/src/main/java/org/osmdroid/samplefragments/SampleFactory.java
@@ -30,7 +30,9 @@ public final class SampleFactory {
                 SampleCustomTileSource.class,
 				SampleAnimatedZoomToLocation.class,
 				SampleWhackyColorFilter.class,
-				SampleCustomIconDirectedLocationOverlay.class
+				SampleCustomIconDirectedLocationOverlay.class,
+				SampleAssetsOnly.class,
+				SampleSqliteOnly.class
         };
 	}
 
