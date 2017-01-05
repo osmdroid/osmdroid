9059857e8240437234be77271c813046ebd3e158
Alex O'Ree <alexoree@apache.org>
2017-01-05 14:59:04 -0500

    NOJIRA  minor logging fixes and javadocs

--- OpenStreetMapViewer/src/main/java/org/osmdroid/samplefragments/SampleFactory.java
+++ OpenStreetMapViewer/src/main/java/org/osmdroid/samplefragments/SampleFactory.java
@@ -10,9 +10,16 @@ import org.osmdroid.samplefragments.cache.SampleCacheDownloaderArchive;
 import org.osmdroid.samplefragments.cache.SampleCacheDownloaderCustomUI;
 import org.osmdroid.samplefragments.cache.SampleJumboCache;
 import org.osmdroid.samplefragments.cache.SampleSqliteOnly;
+import org.osmdroid.samplefragments.data.AsyncTaskDemoFragment;
 import org.osmdroid.samplefragments.data.HeatMap;
+import org.osmdroid.samplefragments.data.SampleGridlines;
 import org.osmdroid.samplefragments.data.SampleMarker;
+import org.osmdroid.samplefragments.data.SampleMilitaryIcons;
+import org.osmdroid.samplefragments.data.SampleOsmPath;
+import org.osmdroid.samplefragments.data.SampleSimpleFastPointOverlay;
 import org.osmdroid.samplefragments.data.SampleSimpleLocation;
+import org.osmdroid.samplefragments.data.SampleWithMinimapItemizedOverlayWithFocus;
+import org.osmdroid.samplefragments.data.SampleWithMinimapItemizedOverlayWithScale;
 import org.osmdroid.samplefragments.events.SampleAnimateTo;
 import org.osmdroid.samplefragments.events.SampleAnimatedZoomToLocation;
 import org.osmdroid.samplefragments.events.SampleLimitedScrollArea;
@@ -20,19 +27,13 @@ import org.osmdroid.samplefragments.events.SampleMapBootListener;
 import org.osmdroid.samplefragments.events.SampleMapEventListener;
 import org.osmdroid.samplefragments.events.SampleZoomToBounding;
 import org.osmdroid.samplefragments.events.ZoomToBoundsOnStartup;
+import org.osmdroid.samplefragments.layouts.MapInAViewPagerFragment;
 import org.osmdroid.samplefragments.layouts.MapInScrollView;
 import org.osmdroid.samplefragments.layouts.SampleFragmentXmlLayout;
 import org.osmdroid.samplefragments.layouts.SampleSplitScreen;
 import org.osmdroid.samplefragments.location.SampleCustomIconDirectedLocationOverlay;
 import org.osmdroid.samplefragments.location.SampleFollowMe;
 import org.osmdroid.samplefragments.location.SampleHeadingCompassUp;
-import org.osmdroid.samplefragments.data.AsyncTaskDemoFragment;
-import org.osmdroid.samplefragments.data.SampleGridlines;
-import org.osmdroid.samplefragments.data.SampleMilitaryIcons;
-import org.osmdroid.samplefragments.data.SampleOsmPath;
-import org.osmdroid.samplefragments.data.SampleWithMinimapItemizedOverlayWithFocus;
-import org.osmdroid.samplefragments.data.SampleWithMinimapItemizedOverlayWithScale;
-import org.osmdroid.samplefragments.data.SampleSimpleFastPointOverlay;
 import org.osmdroid.samplefragments.location.SampleRotation;
 import org.osmdroid.samplefragments.tilesources.SampleAssetsOnly;
 import org.osmdroid.samplefragments.tilesources.SampleCopyrightOverlay;
@@ -45,131 +46,179 @@ import org.osmdroid.samplefragments.tilesources.SampleMapQuest;
 import org.osmdroid.samplefragments.tilesources.SampleOfflineOnly;
 import org.osmdroid.samplefragments.tilesources.SampleOpenSeaMap;
 import org.osmdroid.samplefragments.tilesources.SampleWhackyColorFilter;
-import org.osmdroid.samplefragments.layouts.MapInAViewPagerFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * factory for all examples
  */
 public final class SampleFactory implements ISampleFactory {
 
-	private final Class<? extends BaseSampleFragment>[] mSamples;
+    private final List<Class<? extends BaseSampleFragment>> mSamples = new ArrayList<>();
 
 
-	private static ISampleFactory _instance;
-	public static ISampleFactory getInstance() {
-		if (_instance == null) {
-			_instance = new SampleFactory();
-		}
-		return _instance;
-	}
+    private static ISampleFactory _instance;
 
-	private SampleFactory() {
-		mSamples = new Class[] {
-				//0
-				SampleWithMinimapItemizedOverlayWithFocus.class,
-				//1
-                SampleWithMinimapItemizedOverlayWithScale.class,
-				//2
-                SampleLimitedScrollArea.class,
-				//3
-                SampleFragmentXmlLayout.class,
-				//4
-                SampleOsmPath.class,
-				//5
-                SampleInvertedTiles_NightMode.class,
-				//6
-                SampleOfflineOnly.class,
-				//7
-                SampleAlternateCacheDir.class,
-				//7
-                SampleMilitaryIcons.class,
-				//8
-                SampleMapBox.class,
-				//9
-                SampleJumboCache.class,
-				//10
-                SampleCustomTileSource.class,
-				//11
-				SampleAnimatedZoomToLocation.class,
-				//12
-				SampleWhackyColorFilter.class,
-				//13
-				SampleCustomIconDirectedLocationOverlay.class,
-				//14
-				SampleAssetsOnly.class,
-				//15
-				SampleSqliteOnly.class,
-				//16
-				SampleCacheDownloader.class,
-				//17
-				SampleCacheDownloaderCustomUI.class,
-				//18
-				SampleCacheDownloaderArchive.class,
-				//19
-				SampleGridlines.class,
-				//20
-				SampleMapEventListener.class,
-				//21
-				SampleAnimateTo.class,
-				//22
-				SampleHeadingCompassUp.class,
-				//23
-				SampleSplitScreen.class,
-				//24
-				SampleMapBootListener.class,
-				//25
-				SampleFollowMe.class,
-				//26
-				SampleMapQuest.class,
-				//27
-				SampleHereWeGo.class,
-				//28
-				SampleCustomLoadingImage.class,
-				//29
-				AsyncTaskDemoFragment.class,
-				//30
-				CacheImport.class,
-				//31
-				CachePurge.class,
-				//32
-				SampleZoomToBounding.class,
-				//33
-				MapInAViewPagerFragment.class,
-				//34
-				ZoomToBoundsOnStartup.class,
-				//35
-				SampleSimpleLocation.class,
-                //36
-				SampleSimpleFastPointOverlay.class,
-				//37
-				SampleOpenSeaMap.class,
-				//38
-				SampleMarker.class,
-                //39
-                SampleRotation.class,
-                //40
-                HeatMap.class,
-				//41
-				MapInScrollView.class,
-				//42
-				SampleCopyrightOverlay.class,
+    public static ISampleFactory getInstance() {
+        if (_instance == null) {
+            _instance = new SampleFactory();
+        }
+        return _instance;
+    }
+
+    private SampleFactory() {
+        mSamples.add(//0
+            SampleWithMinimapItemizedOverlayWithFocus.class);
+
+        //1
+        mSamples.add(SampleWithMinimapItemizedOverlayWithScale.class);
+        mSamples.add(
+            //2
+            SampleLimitedScrollArea.class);
+        mSamples.add(
+            //3
+            SampleFragmentXmlLayout.class);
+        mSamples.add(
+            //4
+            SampleOsmPath.class);
+        mSamples.add(
+            //5
+            SampleInvertedTiles_NightMode.class);
+        mSamples.add(
+            //6
+            SampleOfflineOnly.class);
+        mSamples.add(
+            //7
+            SampleAlternateCacheDir.class);
+        mSamples.add(
+            //7
+            SampleMilitaryIcons.class);
+        mSamples.add(
+            //8
+            SampleMapBox.class);
+        mSamples.add(
+            //9
+            SampleJumboCache.class);
+        mSamples.add(
+            //10
+            SampleCustomTileSource.class);
+        mSamples.add(
+            //11
+            SampleAnimatedZoomToLocation.class);
+        mSamples.add(
+            //12
+            SampleWhackyColorFilter.class);
+        mSamples.add(
+            //13
+            SampleCustomIconDirectedLocationOverlay.class);
+        mSamples.add(
+            //14
+            SampleAssetsOnly.class);
+        mSamples.add(
+            //15
+            SampleSqliteOnly.class);
+        mSamples.add(
+            //16
+            SampleCacheDownloader.class);
+        mSamples.add(
+            //17
+            SampleCacheDownloaderCustomUI.class);
+        mSamples.add(
+            //18
+            SampleCacheDownloaderArchive.class);
+        mSamples.add(
+            //19
+            SampleGridlines.class);
+        mSamples.add(
+            //20
+            SampleMapEventListener.class);
+        mSamples.add(
+            //21
+            SampleAnimateTo.class);
+        mSamples.add(
+            //22
+            SampleHeadingCompassUp.class);
+        mSamples.add(
+            //23
+            SampleSplitScreen.class);
+        mSamples.add(
+            //24
+            SampleMapBootListener.class);
+        mSamples.add(
+            //25
+            SampleFollowMe.class);
+        mSamples.add(
+            //26
+            SampleMapQuest.class);
+        mSamples.add(
+            //27
+            SampleHereWeGo.class);
+        mSamples.add(
+            //28
+            SampleCustomLoadingImage.class);
+        mSamples.add(
+            //29
+            AsyncTaskDemoFragment.class);
+        mSamples.add(
+            //30
+            CacheImport.class);
+        mSamples.add(
+            //31
+            CachePurge.class);
+        mSamples.add(
+            //32
+            SampleZoomToBounding.class);
+        mSamples.add(
+            //33
+            MapInAViewPagerFragment.class);
+        mSamples.add(
+            //34
+            ZoomToBoundsOnStartup.class);
+        mSamples.add(
+            //35
+            SampleSimpleLocation.class);
+        mSamples.add(
+            //36
+            SampleSimpleFastPointOverlay.class);
+        mSamples.add(
+            //37
+            SampleOpenSeaMap.class);
+        mSamples.add(
+            //38
+            SampleMarker.class);
+        mSamples.add(
+            //39
+            SampleRotation.class);
+        mSamples.add(
+            //40
+            HeatMap.class);
+        mSamples.add(
+            //41
+            MapInScrollView.class);
+        mSamples.add(
+            //42
+            SampleCopyrightOverlay.class);
 
 
-        };
-	}
+    }
 
-	public BaseSampleFragment getSample(int index) {
-		try {
-			return mSamples[index].newInstance();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    public void addSample(Class<? extends BaseSampleFragment> clz) {
+        mSamples.add(clz);
+    }
 
-	public int count() {
-		return mSamples.length;
-	}
+    public BaseSampleFragment getSample(int index) {
+        try {
+            return mSamples.get(index).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public int count() {
+        return mSamples.size();
+    }
 }
