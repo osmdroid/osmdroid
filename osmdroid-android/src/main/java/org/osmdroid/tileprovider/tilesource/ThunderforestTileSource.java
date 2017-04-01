faf13f02a38713ae5d862b278bbaf86de58c1a59
rjl <rjl@localhost>
2017-04-01 09:36:13 +0100

    UI name vs URL map name

--- osmdroid-android/src/main/java/org/osmdroid/tileprovider/tilesource/ThunderforestTileSource.java
+++ osmdroid-android/src/main/java/org/osmdroid/tileprovider/tilesource/ThunderforestTileSource.java
@@ -19,34 +19,56 @@ public class ThunderforestTileSource  extends OnlineTileSourceBase
     private static final String THUNDERFOREST_MAPID = "THUNDERFOREST_MAPID";
 
     /** the available map types */
-    public static final String CYCLE="cycle";
-    public static final String TRANSPORT="transport";
-    public static final String LANDSCAPE="landscape";
-    public static final String OUTDOORS="outdoors";
-    public static final String TRANSPORT_DARK="transport-dark";
-    public static final String SPINAL_MAP="spinal-map";
-    public static final String PIONEER="pioneer";
-    public static final String MOBILE_ATLAS="mobile-atlas";
-    public static final String NEIGHBOURHOOD="neighbourhood";
+    public static final int CYCLE=0;
+    public static final int TRANSPORT=1;
+    public static final int LANDSCAPE=2;
+    public static final int OUTDOORS=3;
+    public static final int TRANSPORT_DARK=4;
+    public static final int SPINAL_MAP=5;
+    public static final int PIONEER=6;
+    public static final int MOBILE_ATLAS=7;
+    public static final int NEIGHBOURHOOD=8;
 
+    //** map names used in URLs */
+    private static final String[] urlMap = new String[]{
+            "cycle",
+            "transport",
+            "landscape",
+            "outdoors",
+            "transport-dark",
+            "spinal-map",
+            "pioneer",
+            "mobile-atlas",
+            "neighbourhood"};
+
+    //** map names used in UI (eg. menu) */
+    private static final String[] uiMap = new String[]{
+            "CycleMap",
+            "Transport",
+            "Landscape",
+            "Outdoors",
+            "TransportDark",
+            "Spinal",
+            "Pioneer",
+            "MobileAtlas",
+            "Neighbourhood"};
 
     private static final String[] baseUrl = new String[]{
             "https://a.tile.thunderforest.com/{map}/",
             "https://b.tile.thunderforest.com/{map}/",
             "https://c.tile.thunderforest.com/{map}/"};
 
-    private final String mMap;
-    private String mMapId = "";
+    private final int mMap;
+    private String mMapId;
 
     /**
      * creates a new Thunderforest tile source, loading the access token and mapid from the manifest
      * @param ctx
      * @param map choice of map to use (eg. CYCLE)
      */
-    public ThunderforestTileSource(final Context ctx, final String aMap)
+    public ThunderforestTileSource(final Context ctx, final int aMap)
     {
-        //TODO UI name differs from URL name
-	super(aMap, 0, 17, 256, ".png", baseUrl, "Maps © Thunderforest, Data © OpenStreetMap contributors.");
+	super(uiMap[aMap], 0, 17, 256, ".png", baseUrl, "Maps © Thunderforest, Data © OpenStreetMap contributors.");
         mMap=aMap;
         retrieveMapId(ctx);
         //this line will ensure uniqueness in the tile cache
@@ -65,7 +87,7 @@ public class ThunderforestTileSource  extends OnlineTileSourceBase
     @Override
     public String getTileURLString(final MapTile aMapTile)
     {
-        StringBuilder url = new StringBuilder(getBaseUrl().replace("{map}",mMap));
+        StringBuilder url = new StringBuilder(getBaseUrl().replace("{map}",urlMap[mMap]));
         url.append(aMapTile.getZoomLevel());
         url.append("/");
         url.append(aMapTile.getX());
