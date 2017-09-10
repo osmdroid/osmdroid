5c6f758cabdd0e46d71f83f313faa305ef1e5073
Alex O'Ree <alexoree@apache.org>
2017-09-10 19:32:38 -0400

    #701 travis

--- travis2.sh
+++ travis2.sh
@@ -14,63 +14,63 @@
 # expected environment variables
 # API = the numerical android api level with an optional 'g' at the end, which signifies to include the google apis
 # ----------
-sdkmanager --list --verbose
+#sdkmanager --list --verbose
 # add the packages we need to compile osmdroid
-export PACKAGES=$PACKAGES "build-tools;23.0.2 build-tools;23.0.1 build-tools;23.0.3" 
-PACKAGES=$PACKAGES "extra;google;m2repository extra;google;google_play_services"
-PACKAGES=$PACKAGES "extra;android;m2repository"
-PACKAGES=$PACKAGES "platforms;android-23 add-ons;addon-google_apis-google-23"
+export PACKAGES="build-tools;23.0.2 build-tools;23.0.1 build-tools;23.0.3" 
+export PACKAGES=$PACKAGES "extra;google;m2repository extra;google;google_play_services"
+export PACKAGES=$PACKAGES "extra;android;m2repository"
+export PACKAGES=$PACKAGES "platforms;android-23 add-ons;addon-google_apis-google-23"
 
 
 # this part simply setups up some commands to send the sdkmanager
 case "$API" in
 '10')
-    PACKAGES=$PACKAGES "platforms;android-10" 
-	PACKAGES=$PACKAGES "system-images;android-10;default;armeabi-v7a" 
+    export PACKAGES=$PACKAGES "platforms;android-10" 
+	export PACKAGES=$PACKAGES "system-images;android-10;default;armeabi-v7a" 
     ;;
 '10g')
-    PACKAGES=$PACKAGES "platforms;android-10"
-	PACKAGES=$PACKAGES "system-images;android-10;google_apis;armeabi-v7a"
-	PACKAGES=$PACKAGES "add-ons;addon-google_apis-google-19"
+    export PACKAGES=$PACKAGES "platforms;android-10"
+	export PACKAGES=$PACKAGES "system-images;android-10;google_apis;armeabi-v7a"
+	export PACKAGES=$PACKAGES "add-ons;addon-google_apis-google-19"
     ;;
 '15')
-    PACKAGES=$PACKAGES "platforms;android-15" 
-	PACKAGES=$PACKAGES "system-images;android-15;default;armeabi-v7a" 
+    export PACKAGES=$PACKAGES "platforms;android-15" 
+	export PACKAGES=$PACKAGES "system-images;android-15;default;armeabi-v7a" 
     ;;
 '15g')
-    PACKAGES=$PACKAGES "platforms;android-15" 
-	PACKAGES=$PACKAGES "system-images;android-15;google_apis;armeabi-v7a"
-	PACKAGES=$PACKAGES "add-ons;addon-google_apis-google-15"
+    export PACKAGES=$PACKAGES "platforms;android-15" 
+	export PACKAGES=$PACKAGES "system-images;android-15;google_apis;armeabi-v7a"
+	export PACKAGES=$PACKAGES "add-ons;addon-google_apis-google-15"
     ;;
 '19')
-    PACKAGES=$PACKAGES "platforms;android-19" 
-	PACKAGES=$PACKAGES "system-images;android-19;default;armeabi-v7a" 
+    export PACKAGES=$PACKAGES "platforms;android-19" 
+	export PACKAGES=$PACKAGES "system-images;android-19;default;armeabi-v7a" 
     ;;
 '19g')
-    PACKAGES=$PACKAGES  "platforms;android-19" 
-	PACKAGES=$PACKAGES "system-images;android-19;google_apis;armeabi-v7a"
-	PACKAGES=$PACKAGES "add-ons;addon-google_apis-google-19"
+    export PACKAGES=$PACKAGES  "platforms;android-19" 
+	export PACKAGES=$PACKAGES "system-images;android-19;google_apis;armeabi-v7a"
+	export PACKAGES=$PACKAGES "add-ons;addon-google_apis-google-19"
     ;;
 '23')
-    PACKAGES=$PACKAGES  "platforms;android-23" 
-	PACKAGES=$PACKAGES "system-images;android-23;default;armeabi-v7a" 
+    export PACKAGES=$PACKAGES  "platforms;android-23" 
+	export PACKAGES=$PACKAGES "system-images;android-23;default;armeabi-v7a" 
     ;;
 '23g')
-    PACKAGES=$PACKAGES "platforms;android-23" 
-	PACKAGES=$PACKAGES "system-images;android-23;google_apis;armeabi-v7a"
-	PACKAGES=$PACKAGES "add-ons;addon-google_apis-google-23"
+    export PACKAGES=$PACKAGES "platforms;android-23" 
+	export PACKAGES=$PACKAGES "system-images;android-23;google_apis;armeabi-v7a"
+	export PACKAGES=$PACKAGES "add-ons;addon-google_apis-google-23"
     ;;
 '24')
-    PACKAGES=$PACKAGES "platforms;android-24" 
-	PACKAGES=$PACKAGES "system-images;android-24;default;armeabi-v7a" 
+    export PACKAGES=$PACKAGES "platforms;android-24" 
+	export PACKAGES=$PACKAGES "system-images;android-24;default;armeabi-v7a" 
     ;;
 '24g')
-    PACKAGES=$PACKAGES "platforms;android-24" 
-	PACKAGES=$PACKAGES "system-images;android-24;google_apis;armeabi-v7a"
-	PACKAGES=$PACKAGES "add-ons;addon-google_apis-google-24"
+    export PACKAGES=$PACKAGES "platforms;android-24" 
+	export PACKAGES=$PACKAGES "system-images;android-24;google_apis;armeabi-v7a"
+	export PACKAGES=$PACKAGES "add-ons;addon-google_apis-google-24"
     ;;
 *)
-    PACKAGES=$PACKAGES $API was not handled
+    export PACKAGES=$PACKAGES $API was not handled
     ;;
 esac
 
