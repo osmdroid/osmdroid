1790711cc0cf43e92c5eb999c06a81beacb91891
Fabrice Fontaine <fabrice_fontaine@hotmail.com>
2018-04-23 14:41:53 +0200

    bug/#999 - bug fix when the SD card is not available
    Impacted classes:
    * `PreferenceActivity`: handled the case when the SD card is not available
    * `StoragePreferenceFragment`: handled the case when the SD card is not available

--- OpenStreetMapViewer/src/main/java/org/osmdroid/PreferenceActivity.java
+++ OpenStreetMapViewer/src/main/java/org/osmdroid/PreferenceActivity.java
@@ -381,7 +381,7 @@ public class PreferenceActivity extends AppCompatActivity implements View.OnClic
                 //i hate android!
                 for (int i = 0; i < externalFilesDirs.length; i++) {
                     File mount = externalFilesDirs[i];
-                    if (mount.exists() && mount.isDirectory()) {
+                    if (mount != null && mount.exists() && mount.isDirectory()) {
                         boolean writable = StorageUtils.isWritable(mount);
                         if (writable) {
                             boolean alreadyAdded = false;
