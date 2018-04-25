0d627724449db4d720b446a266a981dcc0cace64
Fabrice Fontaine <fabrice_fontaine@hotmail.com>
2018-04-25 18:23:25 +0200

    New interface for init admin tasks to be run during splash screen
    New interface:
    * `SplashScreenable`: `interface` for `class`es that have something a bit slow to perform as init, which could be done during an app splash screen
    
    New class:
    * `DuringSplashScreen`: container of all osmdroid's `SplashScreenable`s - to be called at splash screen time
    
    Impacted class:
    * `SqlTileWriter`: implemented `SplashScreenable` in order to create the index; created method `createIndex` to be used at splash screen and at garbage collection time

---
+++ osmdroid-android/src/main/java/org/osmdroid/util/SplashScreenable.java
@@ -0,0 +1,22 @@
+package org.osmdroid.util;
+
+/**
+ * @since 6.0.2
+ * @author Fabrice Fontaine
+ * Suggested by @InI4 in https://github.com/osmdroid/osmdroid/issues/1005
+ * There are some init admin tasks that may take time.
+ * For the apps that have a splash screen it makes sense to run them during the splash screen.
+ *
+ * BE CAUTIOUS: we cannot expect all apps to have a splash screen,
+ * and some apps prefer to have the fastest start up.
+ *
+ * Recommandations (short version):
+ * - put there actions that are reasonably slow (less than 30 seconds)
+ * - the actions can be run multiple times, but will only do something (and last long) the very first time
+ * - don't expect all apps to have a splash screen and therefore to systematically call those actions
+ * - as a consequence, there's a fair chance you'll have to run the same actions somewhere else in the code
+ */
+public interface SplashScreenable {
+
+    void runDuringSplashScreen();
+}
