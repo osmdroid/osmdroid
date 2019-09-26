d80cc67285b3d0187cfbfe442574d99b1ed5cb2e
monsieurtanuki <fabrice_fontaine@hotmail.com>
2019-09-27 01:12:29 +0200

    bug/#1145 - using the new DirectRenderer class of mapsforge (#1152)
    * bug/#1145 - using the new DirectRenderer class of mapsforge
    
    That fixes the issue.
    
    Not to be merged as is, because we still rely on a `SNAPSHOT` version of `mapsforge`.
    As soon as `mapsforge` issues a new version, the `build.gradle` file must be edited and marginally also a comment in `MapsForgeTileSource` about the `mapsforge` version number.
    
    Impacted files
    * `osmdroid-mapsforge/build.gradle`: now using the latest version of `mapsforge`, that includes the new `DirectRenderer` class that I coded in `mapsforge`
    * `MapsForgeTileSource`: used `DirectRenderer` instead of `DatabaseRenderer`
    
    * Use of new MapsForge's DirectRenderer.TileRefresher
    
    Impacted classes:
    * `MapTileProviderBase`: created method `expireInMemoryCache`, in order to "expire" a MapsForge tile that needs to be refreshed due to new overlapping labels from new neighboring tiles
    * `MapsForgeTileProvider`: added a call to a tile refresher callback whenever a tile needs to be refreshed
    * `MapsForgeTileSource`: method `renderTile` now returns `null` when MapsForge doesn't know the tile - used to be a yellow bitmap instead; created method `addTileRefresher`
    
    * Impact of new mapsforge version
    
    * edited the build.gradle according to the new mapsforge version number
    * marginally edited a comment in MapsForgeTileSource
    
    * #1145 fixes crashing due to out of bounds coordindates
    
    * bug/#1145 - fine-tuning latitude 90/-90 side effect
    
    Impacted class:
    * `MapsForgeTileSource`: fine-tuned method `getBoundsOsmdroid` about the latitude 90/-90 side effect

--- osmdroid-mapsforge/src/main/java/org/osmdroid/mapsforge/MapsForgeTileSource.java
+++ osmdroid-mapsforge/src/main/java/org/osmdroid/mapsforge/MapsForgeTileSource.java
@@ -1,8 +1,6 @@
 package org.osmdroid.mapsforge;
 
 import android.app.Application;
-import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
@@ -12,20 +10,18 @@ import org.mapsforge.core.model.Tile;
 import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
 import org.mapsforge.map.android.graphics.AndroidTileBitmap;
 import org.mapsforge.map.datastore.MultiMapDataStore;
-import org.mapsforge.map.layer.cache.InMemoryTileCache;
-import org.mapsforge.map.layer.labels.TileBasedLabelStore;
-import org.mapsforge.map.layer.renderer.DatabaseRenderer;
+import org.mapsforge.map.layer.renderer.DirectRenderer;
 import org.mapsforge.map.layer.renderer.RendererJob;
 import org.mapsforge.map.layer.hills.HillsRenderConfig;
 import org.mapsforge.map.model.DisplayModel;
 import org.mapsforge.map.reader.MapFile;
-import org.mapsforge.map.reader.ReadBuffer;
 import org.mapsforge.map.rendertheme.InternalRenderTheme;
 import org.mapsforge.map.rendertheme.XmlRenderTheme;
 import org.mapsforge.map.rendertheme.rule.RenderThemeFuture;
 import org.osmdroid.api.IMapView;
 import org.osmdroid.tileprovider.tilesource.BitmapTileSourceBase;
 import org.osmdroid.util.MapTileIndex;
+import org.osmdroid.views.MapView;
 
 import java.io.File;
 
@@ -46,7 +42,7 @@ public class MapsForgeTileSource extends BitmapTileSourceBase {
     private final float scale = DisplayModel.getDefaultUserScaleFactor();
     private RenderThemeFuture theme = null;
     private XmlRenderTheme mXmlRenderTheme = null;
-    private DatabaseRenderer renderer;
+    private DirectRenderer renderer;
     private HillsRenderConfig hillsRenderConfig;
 
     private MultiMapDataStore mapDatabase;
@@ -81,9 +77,11 @@ public class MapsForgeTileSource extends BitmapTileSourceBase {
         //renderer = new DatabaseRenderer(mapDatabase, AndroidGraphicFactory.INSTANCE, tileCache,
         //        new TileBasedLabelStore(tileCache.getCapacityFirstLevel()), true, true);
         // mapsforge0.8.0
-        InMemoryTileCache tileCache = new InMemoryTileCache(2);
-        renderer = new DatabaseRenderer(mapDatabase, AndroidGraphicFactory.INSTANCE, tileCache,
-                new TileBasedLabelStore(tileCache.getCapacityFirstLevel()), true, true, hillsRenderConfig);
+        //InMemoryTileCache tileCache = new InMemoryTileCache(2);
+        //renderer = new DatabaseRenderer(mapDatabase, AndroidGraphicFactory.INSTANCE, tileCache,
+        //        new TileBasedLabelStore(tileCache.getCapacityFirstLevel()), true, true, hillsRenderConfig);
+        // mapsforge0.11.0
+        renderer = new DirectRenderer(mapDatabase, AndroidGraphicFactory.INSTANCE, true, hillsRenderConfig);
 
         minZoom = MIN_ZOOM;
         maxZoom = renderer.getZoomLevelMax();
@@ -106,9 +104,11 @@ public class MapsForgeTileSource extends BitmapTileSourceBase {
 
     public org.osmdroid.util.BoundingBox getBoundsOsmdroid(){
         BoundingBox boundingBox = mapDatabase.boundingBox();
-        org.osmdroid.util.BoundingBox bounds = new org.osmdroid.util.BoundingBox(boundingBox.maxLatitude, boundingBox.maxLongitude,
-            boundingBox.minLatitude, boundingBox.minLongitude);
-        return bounds;
+        final double latNorth= Math.min(MapView.getTileSystem().getMaxLatitude(),boundingBox.maxLatitude);
+        final double latSouth= Math.max(MapView.getTileSystem().getMinLatitude(),boundingBox.minLatitude);
+        return new org.osmdroid.util.BoundingBox(
+                latNorth, boundingBox.maxLongitude,
+                latSouth, boundingBox.minLongitude);
     }
 
     /**
@@ -200,10 +200,7 @@ public class MapsForgeTileSource extends BitmapTileSourceBase {
         } catch (Exception ex) {
             Log.d(IMapView.LOGTAG, "###################### Mapsforge tile generation failed", ex);
         }
-        //Make the bad tile easy to spot
-        Bitmap bitmap = Bitmap.createBitmap(TILE_SIZE_PIXELS, TILE_SIZE_PIXELS, Bitmap.Config.RGB_565);
-        bitmap.eraseColor(Color.YELLOW);
-        return new BitmapDrawable(bitmap);
+        return null;
     }
 
     public static void createInstance(Application app) {
@@ -218,5 +215,14 @@ public class MapsForgeTileSource extends BitmapTileSourceBase {
         if (mapDatabase!=null)
             mapDatabase.close();
         mapDatabase=null;
+    }
+
+    /**
+     * @since 6.0.3
+     */
+    public void addTileRefresher(DirectRenderer.TileRefresher pDirectTileRefresher) {
+        if (pDirectTileRefresher != null) {
+            renderer.addTileRefresher(pDirectTileRefresher);
+        }
     }
 }
