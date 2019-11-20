fa6666b7347e861e6e761f0a7e6fc6a5574cf470
Matthias Dittmer <mail@matthiasdittmer.de>
2019-11-20 13:31:14 +0100

    Adding color mappings for saturation and luminance. Saturation will map from gray to full color (0.0f - 1.0f). Luminance will map from full luminance at 0.5f to full black at 0.0f oder full white at 1.0f.

---
+++ osmdroid-android/src/main/java/org/osmdroid/views/overlay/advancedpolyline/ColorMappingVariationLuminance.java
@@ -0,0 +1,34 @@
+package org.osmdroid.views.overlay.advancedpolyline;
+
+import java.util.ArrayList;
+
+/**
+ * Color mapping for luminance variation.
+ * @author Matthias Dittmer
+ */
+public class ColorMappingVariationLuminance extends ColorMappingVariation {
+
+    private float mHue;
+    private float mSaturation;
+
+    public ColorMappingVariationLuminance(float scalarStart, float scalarEnd,
+                                           float luminanceStart, float luminanceEnd,
+                                           float hue, float saturation) {
+
+        // do basic clipping for hue value
+        // please note: end can be lower than start for inverse mapping
+        luminanceStart = ColorHelper.constrain(luminanceStart, 0.0f, 1.0f);
+        luminanceEnd = ColorHelper.constrain(luminanceEnd, 0.0f, 1.0f);
+
+        // do clipping for saturation and luminance
+        mHue = ColorHelper.constrain(hue, 0.0f, 360.0f);
+        mSaturation = ColorHelper.constrain(saturation, 0.0f, 1.0f);
+
+        super.init(scalarStart, scalarEnd, luminanceStart, luminanceEnd);
+    }
+
+    public void addPoint(float scalar) {
+        // create mapped luminance value
+        super.addToLists(scalar, mHue, mSaturation, mapScalar(scalar));
+    }
+}
---
+++ osmdroid-android/src/main/java/org/osmdroid/views/overlay/advancedpolyline/ColorMappingVariationSaturation.java
@@ -0,0 +1,34 @@
+package org.osmdroid.views.overlay.advancedpolyline;
+
+import java.util.ArrayList;
+
+/**
+ * Color mapping for saturation variation.
+ * @author Matthias Dittmer
+ */
+public class ColorMappingVariationSaturation extends ColorMappingVariation{
+
+    private float mHue;
+    private float mLuminance;
+
+    public ColorMappingVariationSaturation(float scalarStart, float scalarEnd,
+                                    float saturationStart, float saturationEnd,
+                                           float hue, float luminance) {
+
+        // do basic clipping for hue value
+        // please note: end can be lower than start for inverse mapping
+        saturationStart = ColorHelper.constrain(saturationStart, 0.0f, 1.0f);
+        saturationEnd = ColorHelper.constrain(saturationEnd, 0.0f, 1.0f);
+
+        // do clipping for saturation and luminance
+        mHue = ColorHelper.constrain(hue, 0.0f, 360.0f);
+        mLuminance = ColorHelper.constrain(luminance, 0.0f, 1.0f);
+
+        super.init(scalarStart, scalarEnd, saturationStart, saturationEnd);
+    }
+
+    public void addPoint(float scalar) {
+        // create mapped saturation value
+        super.addToLists(scalar, mHue, mapScalar(scalar), mLuminance);
+    }
+}
