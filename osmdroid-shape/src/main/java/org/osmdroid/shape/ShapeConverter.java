ee5d0e6ed6683b539e29e9a0b5858c36f90bd2ce
Sun Jiao <xzzzf.dsx@gmail.com>
2020-04-17 22:59:13 +0800

    fix out of range & color, line, bubble customize
    fix out of range problem; 
    fill color, line color, line width customize; 
    title, snippet, sub-description of bubble customize.

--- osmdroid-shape/src/main/java/org/osmdroid/shape/ShapeConverter.java
+++ osmdroid-shape/src/main/java/org/osmdroid/shape/ShapeConverter.java
@@ -1,5 +1,7 @@
 package org.osmdroid.shape;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.Log;
 
 import net.iryndin.jdbf.core.DbfRecord;
@@ -37,9 +39,24 @@ import java.util.List;
  * @since 6.1.0
  */
 
-public class ShapeConverter {
+public class GridShapeConverter {
 
-    public static List<Overlay> convert(MapView map, File file, ValidationPreferences prefs) throws Exception {
+    /**
+     *
+     * @param map the MapView to which these overlays will be added.
+     * @param file the shape file to be converted.
+     * @param prefs  allows the client to relax the level of validation when reading a shape file.
+     * @param fillColor fill color of polygon.
+     * @param outLineColor out line color of polyOverlay.
+     * @param width out line width of polyOverlay.
+     * @param getBubbleText customize titles, snippets, and sub-descriptions of bubbles.
+     *                      remember to handling bounding box if your shape file has both points and poly-overlays.
+     *                      please also mind that getSnippet is called before getTitle, and before getSubDescription.
+     * @return
+     * @throws Exception
+     */
+    public static List<Overlay> convert(MapView map, File file, ValidationPreferences prefs,
+                                        int fillColor, int outLineColor, int width, GetBubbleText getBubbleText) throws Exception {
         List<Overlay> folder=new ArrayList<>();
 
         FileInputStream is = null;
@@ -66,12 +83,13 @@ public class ShapeConverter {
                     case POINT: {
                         PointShape aPoint = (PointShape) s;
                         Marker m = new Marker(map);
-                        m.setPosition(fixCritical(new GeoPoint(aPoint.getY(), aPoint.getX())));
+                        m.setPosition(fixOutOfRange(new GeoPoint(aPoint.getY(), aPoint.getX())));
                         if (metadata != null) {
                             metadata.setStringCharset(Charset.defaultCharset());
-                            m.setSnippet(metadata.toMap().toString());
-                            m.setTitle(getSensibleTitle(m.getSnippet()));
+                            String metaString = metadata.toMap().toString();
 
+                            m.setSnippet(getBubbleText.getSnippet(metaString, ""));
+                            m.setTitle(getBubbleText.getTitle(metaString, ""));
                         }
                         folder.add(m);
                     }
@@ -83,28 +101,32 @@ public class ShapeConverter {
 
                         for (int i = 0; i < aPolygon.getNumberOfParts(); i++) {
                             Polygon polygon = new Polygon(map);
-                            if (metadata != null) {
-                                metadata.setStringCharset(Charset.defaultCharset());
-
-                                polygon.setSnippet(metadata.toMap().toString());
-                                polygon.setTitle(getSensibleTitle(polygon.getSnippet()));
-                            }
 
                             PointData[] points = aPolygon.getPointsOfPart(i);
                             List<GeoPoint> pts = new ArrayList<>();
 
                             for (PointData p : points) {
-                                GeoPoint pt = new GeoPoint(p.getY(), p.getX());
-                                pts.add(fixCritical(pt));
+                                GeoPoint pt = fixOutOfRange(new GeoPoint(p.getY(), p.getX()));
+                                pts.add(pt);
                             }
                             pts.add(pts.get(0));    //force the polygon to close
 
-                            polygon.setPoints(pts);
+                            polygon.setPoints(pts); //points out of range should be fixed before this line.
                             final BoundingBox boundingBox = polygon.getBounds();
-                            polygon.setSubDescription(boundingBox.toString());
+                            String boxString = boundingBox.toString();
+                            if (metadata != null) {
+                                metadata.setStringCharset(Charset.defaultCharset());
+                                String metaString = metadata.toMap().toString();
+
+                                polygon.setSnippet(getBubbleText.getSnippet(metaString, boxString));
+                                polygon.setTitle(getBubbleText.getTitle(metaString, boxString));
+                                polygon.setSubDescription(getBubbleText.getSubDescription(metaString, boxString));
+                            }
+                            polygon.getFillPaint().setColor(fillColor); //set fill color
+                            Paint paint = polygon.getOutlinePaint();
+                            paint.setStrokeWidth(width);
+                            paint.setColor(outLineColor);
                             folder.add(polygon);
-
-
                         }
                     }
 
@@ -113,22 +135,31 @@ public class ShapeConverter {
                         PolylineShape polylineShape = (PolylineShape) s;
                         for (int i = 0; i < polylineShape.getNumberOfParts(); i++) {
                             Polyline line = new Polyline(map);
-                            if (metadata != null) {
-                                metadata.setStringCharset(Charset.defaultCharset());
-                                line.setSnippet(metadata.toMap().toString());
-                                line.setTitle(getSensibleTitle(line.getSnippet()));
-                            }
+
                             PointData[] points = polylineShape.getPointsOfPart(i);
                             List<GeoPoint> pts = new ArrayList<>();
 
                             for (PointData p : points) {
-                                GeoPoint pt = new GeoPoint(p.getY(), p.getX());
-                                pts.add(fixCritical(pt));
+                                GeoPoint pt = fixOutOfRange(new GeoPoint(p.getY(), p.getX()));
+                                pts.add(pt);
                             }
-                            line.setPoints(pts);
+
+                            line.setPoints(pts);//points out of range should be fixed before this line.
+                            final BoundingBox boundingBox = line.getBounds();
+                            String boxString = boundingBox.toString();
+                            if (metadata != null) {
+                                metadata.setStringCharset(Charset.defaultCharset());
+                                String metaString = metadata.toMap().toString();
+
+                                line.setSnippet(getBubbleText.getSnippet(metaString, boxString));
+                                line.setTitle(getBubbleText.getTitle(metaString, boxString));
+                                line.setSubDescription(getBubbleText.getSubDescription(metaString, boxString));
+                            }
+                            Paint paint = line.getOutlinePaint();
+                            paint.setStrokeWidth(width);
+                            paint.setColor(outLineColor);
                             folder.add(line);
                         }
-
                     }
                     break;
                     case MULTIPOINT: {
@@ -137,18 +168,16 @@ public class ShapeConverter {
                         PointData[] points = aPoint.getPoints();
                         for (PointData p : points) {
                             Marker m = new Marker(map);
-                            m.setPosition(fixCritical(new GeoPoint(p.getY(), p.getX())));
+                            m.setPosition(fixOutOfRange(new GeoPoint(p.getY(), p.getX())));
                             if (metadata != null) {
                                 metadata.setStringCharset(Charset.defaultCharset());
-                                m.setSnippet(metadata.toMap().toString());
-                                m.setTitle(getSensibleTitle(m.getSnippet()));
+                                String metaString = metadata.toMap().toString();
 
-
+                                m.setSnippet(getBubbleText.getSnippet(metaString, ""));
+                                m.setTitle(getBubbleText.getTitle(metaString, ""));
                             }
                             folder.add(m);
                         }
-
-
                     }
                     break;
                     default:
@@ -174,29 +203,68 @@ public class ShapeConverter {
         return folder;
     }
 
-    private static String getSensibleTitle(String snippet) {
-        if (snippet.length() > 100) {
-            return snippet.substring(0, 96) + "...";
+    private static GetBubbleText defaultBubbleText = new GetBubbleText() {
+        @Override
+        public String getTitle(String metadata, String boundingBox) {
+            String snippet = getSnippet(metadata, boundingBox);
+            if (snippet.length() > 100) {
+                return snippet.substring(0, 96) + "...";
+            }
+            return snippet;
         }
-        return snippet;
-    }
+
+        @Override
+        public String getSubDescription(String metadata, String boundingBox) {
+            return boundingBox;
+        }
+
+        @Override
+        public String getSnippet(String metadata, String boundingBox) {
+            return metadata;
+        }
+    };
 
     public static List<Overlay>  convert(MapView map, File file) throws Exception {
         ValidationPreferences pref = new ValidationPreferences();
         pref.setMaxNumberOfPointsPerShape(200000);
         return convert(map, file, pref);
     }
-    
-    private static GeoPoint fixCritical (GeoPoint point){
+
+    public static List<Overlay> convert(MapView map, File file, ValidationPreferences pref) throws Exception {
+        return convert(map, file, pref, Color.TRANSPARENT, Color.BLACK, 5, defaultBubbleText);
+    }
+
+    public static List<Overlay> convert(MapView map, File file, int fillColor, int outLineColor, int width) throws Exception {
+        ValidationPreferences pref = new ValidationPreferences();
+        pref.setMaxNumberOfPointsPerShape(200000);
+        return convert(map, file, pref, fillColor, outLineColor, width, defaultBubbleText);
+    }
+
+    public static List<Overlay> convert(MapView map, File file, GetBubbleText getBubbleText) throws Exception {
+        ValidationPreferences pref = new ValidationPreferences();
+        pref.setMaxNumberOfPointsPerShape(200000);
+        return convert(map, file, pref, Color.TRANSPARENT, Color.BLACK, 5, getBubbleText);
+    }
+
+    public static List<Overlay> convert(MapView map, File file, int fillColor, int outLineColor, int width, GetBubbleText getBubbleText) throws Exception {
+        ValidationPreferences pref = new ValidationPreferences();
+        pref.setMaxNumberOfPointsPerShape(200000);
+        return convert(map, file, pref, fillColor, outLineColor, width, getBubbleText);
+    }
+
+    private static GeoPoint fixOutOfRange(GeoPoint point){
         if (point.getLatitude()>90.00)
             point.setLatitude(90.00);
         else if (point.getLatitude()<-90.00)
             point.setLatitude(-90.00);
-        if (point.getLongitude()>180.00 && point.getLongitude()<180.01)
-            point.setLongitude(180.00);
-        else if (point.getLongitude()<-180.00 && point.getLongitude()>-180.01)
-            point.setLongitude(-180.00);
 
+        if (point.getLongitude()>180.00){
+            double longitude = point.getLongitude() -360.00;
+            point.setLongitude(longitude);
+        } else if (point.getLongitude()<-180.00){
+            double longitude = point.getLongitude() +360.00;
+            point.setLongitude(longitude);
+        }
         return point;
     }
 }
