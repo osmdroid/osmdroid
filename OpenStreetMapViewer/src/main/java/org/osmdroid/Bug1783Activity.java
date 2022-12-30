0d1e5cb461c018be2e1e9c47f24ea4348c8d2bfa
James Addison <jay@jp-hosting.net>
2022-12-30 14:21:50 +0000

    Revert "Revert "#1783 resolves a NPE related to disableFollowLocation from MyLocationNewOverlay, the rest of the changes are test code to reproduce the issue. Note: i tried to rig up an automated test for this but i was not successful. (#1811)""
    This reverts commit 46cc59b14e30af27fd1ed2b23bb160045d01f1a2.
    
    Conflicts:
    	OpenStreetMapViewer/src/main/AndroidManifest.xml
    
    Co-authored-by: seadowg
    Co-authored-by: spyhunter99 <spyhunter99@users.noreply.github.com>

---
+++ OpenStreetMapViewer/src/main/java/org/osmdroid/Bug1783Activity.java
@@ -0,0 +1,49 @@
+package org.osmdroid;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.osmdroid.bugtestfragments.Bug1783MyLocationOverlayNPE;
+import org.osmdroid.model.IBaseActivity;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentActivity;
+
+public class Bug1783Activity extends FragmentActivity implements IBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bug1783);
+        Button button = findViewById(R.id.bug1782Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                final DialogFragment dialog = new Bug1783MyLocationOverlayNPE();
+
+                dialog.show(Bug1783Activity.this.getSupportFragmentManager(), "tag");
+               /* try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+
+                }
+                Bug1783Activity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        dialog.dismiss();
+
+                    }
+                });*/
+
+
+
+            }
+        });
+    }
+
+    @Override
+    public String getActivityTitle() {
+        return "My location overview dialog fragment";
+    }
+}
