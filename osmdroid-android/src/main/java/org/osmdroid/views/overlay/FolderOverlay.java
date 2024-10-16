a57f367dc77c9b29e3d1ae073422ae50200bec1d
hestonic <daniilgeroldovich@gmail.com>
2024-10-16 12:50:41 +0500

    Add documentation for BaseFolderOverlay and its child classes (#2063)

--- osmdroid-android/src/main/java/org/osmdroid/views/overlay/FolderOverlay.java
+++ osmdroid-android/src/main/java/org/osmdroid/views/overlay/FolderOverlay.java
@@ -3,7 +3,11 @@ package org.osmdroid.views.overlay;
 import android.content.Context;
 
 /**
- * A {@link org.osmdroid.views.overlay.FolderOverlay} is just a group of other {@link org.osmdroid.views.overlay.Overlay}s.
+ * A {@link org.osmdroid.views.overlay.FolderOverlay} is a group of other {@link org.osmdroid.views.overlay.Overlay}s,
+ * allowing for easier management of multiple overlays.
+ *
+ * <p>This class inherits from {@link BaseFolderOverlay} and serves as a container for multiple overlays, such as markers,
+ * polygons, and polylines, enabling their combined management (e.g., drawing, interaction, visibility).</p>
  *
  * <img alt="Class diagram around Marker class" width="686" height="413" src='./doc-files/marker-classes.png' />
  *
@@ -12,13 +16,37 @@ import android.content.Context;
 public class FolderOverlay extends BaseFolderOverlay<Overlay> {
 
     /**
-     * Use {@link #FolderOverlay()} instead
+     * Deprecated constructor that accepts a {@link Context}.
+     *
+     * <p>Use {@link #FolderOverlay()} instead. This constructor is kept for backward compatibility.</p>
+     *
+     * @param ctx the context in which this overlay operates.
+     * @deprecated Use {@link #FolderOverlay()} instead.
      */
     @Deprecated
     public FolderOverlay(Context ctx) {
         this();
     }
 
+    /**
+     * Default constructor for creating a {@link FolderOverlay}.
+     *
+     * <p>This constructor initializes an empty folder overlay that can contain other {@link Overlay} objects.</p>
+     *
+     * <p>Example of initialization:</p>
+     * <pre>
+     * // Creating a FolderOverlay
+     * FolderOverlay folderOverlay = new FolderOverlay();
+     *
+     * // Creating and adding various overlays (e.g., markers, polygons)
+     * Marker marker = new Marker(mapView);
+     * marker.setPosition(new GeoPoint(48.8588443, 2.2943506)); // Set marker to Eiffel Tower
+     * folderOverlay.add(marker);
+     *
+     * // Add the folder overlay to the map
+     * mapView.getOverlays().add(folderOverlay);
+     * </pre>
+     */
     public FolderOverlay() {
         super(Overlay.class);
     }
--- osmdroid-android/src/main/java/org/osmdroid/views/overlay/TypedFolderOverlay.java
+++ osmdroid-android/src/main/java/org/osmdroid/views/overlay/TypedFolderOverlay.java
@@ -4,14 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A {@link org.osmdroid.views.overlay.FolderOverlay} is just a group of other {@link org.osmdroid.views.overlay.Overlay}s.
+ * A generic folder overlay that groups multiple overlays of a specific type.
  *
- * <img alt="Class diagram around Marker class" width="686" height="413" src='./doc-files/marker-classes.png' />
+ * <p>{@link TypedFolderOverlay} extends {@link BaseFolderOverlay} and allows for type-specific
+ * management of overlays. This class ensures that only overlays of type {@code T} can be added
+ * to the folder, providing type safety and better control over the contained overlays.</p>
+ *
+ * @param <T> the specific type of {@link Overlay} that this folder manages.
  *
  * @author Daniil Timofeev (hestonic)
  */
 public class TypedFolderOverlay<T extends Overlay> extends BaseFolderOverlay<T> {
 
+    /**
+     * Constructs a new {@link TypedFolderOverlay} with the specified overlay type.
+     *
+     * <p>This constructor ensures that only overlays of the specified type can be added to this folder overlay.</p>
+     *
+     * <p>Example of initialization:</p>
+     * <pre>
+     * // Creating a TypedFolderOverlay for Marker overlays
+     * TypedFolderOverlay<Marker> markerFolder = new TypedFolderOverlay<>(Marker.class);
+     *
+     * // Adding a marker to the folder
+     * Marker marker = new Marker(mapView);
+     * marker.setPosition(new GeoPoint(48.8588443, 2.2943506)); // Set marker to Eiffel Tower
+     * markerFolder.add(marker);
+     *
+     * // Adding the folder to the map
+     * mapView.getOverlays().add(markerFolder);
+     * </pre>
+     *
+     * @param clazz the class of the overlay type {@code T}.
+     */
     public TypedFolderOverlay(Class<T> clazz) {
         super(clazz);
     }
